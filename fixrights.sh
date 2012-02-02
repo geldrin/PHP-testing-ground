eabc1e885421d13d20b7aab369b3051e715b775b
Dotsamazing <dam@stream.teleconnect.hu>
2012-02-02 18:37:03 +0100

    fixrights es pull script

---
+++ fixrights.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+chown -R dam:www-data /var/www/dev.video.teleconnect.hu
+chmod -R g+w /var/www/dev.video.teleconnect.hu
+
+chown -R dam:www-data /var/www/video.teleconnect.hu
+chmod -R g+w /var/www/video.teleconnect.hu
