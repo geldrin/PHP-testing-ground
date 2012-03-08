86c3f2c295a5af090caef5a8be0c503e2caa5893
root <root@stream.teleconnect.hu>
2012-03-08 15:04:50 +0100

    fixrights+

--- fixrights.sh
+++ fixrights.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 chown -R dam:www-data /var/www/dev.video.teleconnect.hu
 chmod -R g+w /var/www/dev.video.teleconnect.hu
+chown -R xtro:www-data /var/www/dev.video.teleconnect.hu/httpdocs/flash/
 
 chown -R dam:www-data /var/www/video.teleconnect.hu
 chmod -R g+w /var/www/video.teleconnect.hu
+chown -R xtro:www-data /var/www/video.teleconnect.hu/httpdocs/flash/
