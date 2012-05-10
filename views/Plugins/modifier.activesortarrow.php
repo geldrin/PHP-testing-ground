a7f90107a321954336ce841ddaaa285bbb1e57f1
Peter SZTANOJEV <sztanpet@gmail.com>
2012-05-10 12:48:33 +0200

    rendezes ne ugraljon

--- views/Plugins/modifier.activesortarrow.php
+++ views/Plugins/modifier.activesortarrow.php
@@ -2,16 +2,16 @@
 
 function smarty_modifier_activesortarrow( $string, $searchfor, $currentorder = null ) {
   
+  $staticuri = \Bootstrap::getInstance()->getSmarty()->get_template_vars('STATIC_URI');
   if ( strpos( $currentorder, $searchfor ) === 0 ) {
     
-    $staticuri = \Bootstrap::getInstance()->getSmarty()->get_template_vars('STATIC_URI');
     $string = str_replace('UP', '<img src="' . $staticuri . 'images/sortarrow_up.png" />', $string );
     $string = str_replace('DN', '<img src="' . $staticuri . 'images/sortarrow_down.png" />', $string );
     
   } else {
     
-    $string = str_replace('UP', '<span class="spacer"></span>', $string );
-    $string = str_replace('DN', '<span class="spacer"></span>', $string );
+    $string = str_replace('UP', '<img src="' . $staticuri . 'images/sortarrow_up_gray.png" />', $string );
+    $string = str_replace('DN', '<img src="' . $staticuri . 'images/sortarrow_down_gray.png" />', $string );
     
   }
   
