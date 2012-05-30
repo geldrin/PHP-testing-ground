80f46fe23090467e8794d8fad8f81d4ca9a5aea7
Dots Amazing <dam@dotsamazing.com>
2012-05-30 17:26:53 +0200

    fixrights update

--- fixrights.sh
+++ fixrights.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-chown -R dam:cms /var/www/dev.video.teleconnect.hu
-chmod -R g+w /var/www/dev.video.teleconnect.hu
-chown -R xtro:cms /var/www/dev.video.teleconnect.hu/httpdocs/flash/
+chown -R dam:cms /var/www/dev.videosquare.eu
+chmod -R g+w /var/www/dev.videosquare.eu
+chown -R xtro:cms /var/www/dev.videosquare.eu/httpdocs/flash/
 
 chown -R dam:cms /var/www/videosquare.eu
 chmod -R g+w /var/www/videosquare.eu
