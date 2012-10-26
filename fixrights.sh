7ccbc6c25a5b3ae5bd430a1b2d4c5ee094073d44
Peter SZTANOJEV <sztanpet@gmail.com>
2012-10-26 15:13:01 +0200

    data/cache konyvtar chown www-datanak

--- fixrights.sh
+++ fixrights.sh
@@ -2,7 +2,9 @@
 chown -R dam:cms /var/www/dev.videosquare.eu
 chmod -R g+w /var/www/dev.videosquare.eu
 chown -R xtro:cms /var/www/dev.videosquare.eu/httpdocs/flash/
+chown -R www-data:www-data /var/www/dev.videosquare.eu/data/cache
 
 chown -R dam:cms /var/www/videosquare.eu
 chmod -R g+w /var/www/videosquare.eu
 chown -R xtro:cms /var/www/videosquare.eu/httpdocs/flash/
+chown -R www-data:www-data /var/www/videosquare.eu/data/cache
