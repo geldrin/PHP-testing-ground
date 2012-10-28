8a1e7ccf0234f99b1f8a36ac2c9ebce1ce1fec23
Kovacs Andras <Andras.Kovacs@teleconnect.hu>
2012-10-28 11:04:07 +0100

    DB backup final

--- resources/db_backup/vsq_db_backup.sh
+++ resources/db_backup/vsq_db_backup.sh
@@ -13,25 +13,20 @@ db_backup_dirs[0]=/srv/storage/dev.videosquare.eu/backup/db
 db_backup_dirs[1]=/srv/storage/videosquare.eu/backup/db
 
 date=`date +"%Y%m%d_%H%M%S"`
-echo "MySQL backup started: ${date}"
 
 for i in ${!db_backup[*]}
 do
-	echo "Backing up DB: ${db_backup[${i}]}"
 
 	# Remove backups older than $days_keep
 	directory=${db_backup_dirs[${i}]}
-	echo " Removing backups older than ${days_keep} days from: ${directory}"
-#	find ${directory}* -mtime +$days_keep -exec rm -f {} \; 2> /dev/null
-	find ${directory}* -mtime +$days_keep
+	find ${directory} -mtime +$days_keep -exec rm -f {} \; 2> /dev/null
 
 	# mysqldump
 	filename=${db_backup[${i}]}_${date}.sql
-	echo " Target filename: ${directory}/${filename}"
 	/usr/bin/mysqldump -u ${mysql_user} -p"${mysql_pwd}" ${db_backup[${i}]} > /tmp/${filename}
 
 	# targz dump file
 	cd /tmp
-	tar -cvzf ${directory}/${filename}.tar.gz ${filename}
+	tar -cvzf ${directory}/${filename}.tar.gz ${filename} 2> /dev/null
 
 done
