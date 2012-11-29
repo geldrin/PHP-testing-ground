d60af21198803dbd33704078fef85abc534a1d0e
Peter SZTANOJEV <sztanpet@gmail.com>
2012-11-29 14:20:15 +0100

    [in progress: 342] Player: nem működik az RTMPT fallback

--- config.php
+++ config.php
@@ -116,7 +116,7 @@ $config = array(
     'sechttpurl'        => 'http://stream.videosquare.eu/vsq/_definst_/',
     
     'rtmpurl'           => 'rtmp://stream.videosquare.eu:1935/vsq/',
-    'secrtmpsurl'       => 'rtmps://stream.videosquare.eu:1935/vsqsec/',
+    'secrtmpsurl'       => 'rtmps://stream.videosquare.eu/vsqsec/',
     'secrtmpurl'        => 'rtmpe://stream.videosquare.eu:1935/vsqsec/',
     
     'rtmpturl'          => 'rtmpt://stream.videosquare.eu:80/vsq/',
