3033381ddd4dd59beed2dfbe45676ca7978efa20
Peter SZTANOJEV <sztanpet@gmail.com>
2013-04-02 14:52:32 +0200

    [completed: 459] Nyelvváltás hiba: magyarról angolra és vissza

--- views/Plugins/modifier.changelanguage.php
+++ views/Plugins/modifier.changelanguage.php
@@ -3,14 +3,28 @@ include_once('modifier.uri.php');
 
 function smarty_modifier_changelanguage( $url, $language ) {
   
-  $organization = \Bootstrap::getInstance()->getSmarty()->get_template_vars('organization');
-  $baseuri      = smarty_modifier_uri( $organization, 'base' );
+  $baseuri = \Bootstrap::getInstance()->getSmarty()->get_template_vars('BASE_URI');
   
   if ( strlen( $url ) < strlen( $baseuri ) )
-    return $url . "/$language/";
+    $newurl = $url . "/$language/";
   elseif ( strlen( $url ) == strlen( $baseuri ) )
-    return $url . "$language/";
+    $newurl = $url . "$language/";
   else
-    return preg_replace( '/^' . preg_quote( $baseuri, '/' ) . '([a-z]{2})\//', $baseuri . $language . '/', $url );
+    $newurl = preg_replace( '/^' . preg_quote( $baseuri, '/' ) . '([a-z]{2})\//', $baseuri . $language . '/', $url );
+  
+  $pos = strpos( $newurl, '?');
+  if ( $pos === false )
+    return $newurl;
+  
+  parse_str( substr( $newurl, $pos + 1 ), $params );
+  if ( isset( $params['forward'] ) and $params['forward'] )
+    $params['forward'] = smarty_modifier_changelanguage( $params['forward'], $language );
+  else
+    return $newurl;
+  
+  $newurl = substr( $newurl, 0, $pos );
+  $newurl .= '?' . http_build_query( $params );
+  
+  return $newurl;
   
 }
