d15e77e7e9d0db5aacf104ada467b9d021399847
Peter SZTANOJEV <sztanpet@gmail.com>
2013-04-27 14:16:16 +0200

    pager valtoztatasok

--- modules/Visitor/Paging.php
+++ modules/Visitor/Paging.php
@@ -9,8 +9,24 @@ abstract class Paging extends \Springboard\Controller\Paging {
     
     parent::setupPager();
     
-    if ( $this->pager )
+    if ( $this->pager ) {
+      
+      $this->pager->perpageformmethod = 'get';
       $this->pager->divider = ' <span class="divider">|</span>';
+      
+    }
+    
+  }
+  
+  protected function needPager() {
+    
+    if ( $this->perpageselector )
+      return true;
+    
+    if ( $this->itemcount > $this->perpage )
+      return true;
+    else
+      return false;
     
   }
   
