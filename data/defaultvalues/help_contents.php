fee132bc19e3cab3eb6e7d4d9a76c3146b4a3607
Dots Amazing <dam@dotsamazing.com>
2013-06-10 09:11:04 +0200

    defaultvalues import, nem ignoraljuk oket tobbe

---
+++ data/defaultvalues/help_contents.php
@@ -0,0 +1,13 @@
+<?php
+return array (
+  0 => 
+  array (
+    'title' => 'Feltöltés súgó',
+	'titleen' => 'Upload help',
+	'shortname' => 'recordings_upload',
+    'title_stringid' => 0,
+    'body' => '<p>A felvétel feltöltéséhez elsőként válassza ki a felvétel eredeti nyelvét!</p><p>Az <strong>"Állományok kiválasztása"</strong> gombra kattintva válassza ki a feltölteni kívánt felvételeket! Kérjük, figyelmesen olvassa el a <strong>"Felhasználási feltételek"</strong> oldalt, majd amennyiben elfogadja a tartalmát, hagyja jóvá a dokumentumot! A feltöltést a <strong>"Feltöltés kezdése"</strong> gomb megnyomásával indíthatja el.</p><p>Amennyiben rendelkezik <strong>HTML5</strong> böngészővel, használja a "fogd és vidd" (drag and drop) módszert! A fájlkezelő alkalmazásban válassza ki a feltölteni kívánt állományokat, majd dobja őket erre a lapra! Modern, HTML5 képes böngésző esetén egyszerre több állományt is feltölthet, valamint megszakadt feltöltéseit 1 héten át megőrizzük Önnek, így bármikor folytathatja azokat.</p>',
+    'bodyen' => '<p>To start upload a media file, select original language of the recording.</p><p>By clicking <strong>"Select files"</strong> select the recordings to be uploaded. Before starting the upload process, please read <strong>"Terms of Service"</strong> document and in case you agree with its content, approve it. To start upload, press <strong>"Start upload"</strong> button.</p><p>In case you have a <strong>HTML5</strong> enabled browser, use drag and drop technique to add files. In your file browser select media files and drop them onto this webpage. With a HTML5 enabled browser you can upload more files at a time. In addition, you can continue uploading your uncompleted recordings within a week from first starting the process.</p>',
+    'body_stringid' => 0,
+  ),
+);
