374f83a55e9328be9248096b0ad36837cf81f2ff
Andras K <andras.kovacs@teleconnect.hu>
2013-07-05 15:54:57 +0200

    DB: avatarsourceip, wowza monitoring mezok

---
+++ resources/dbmigration/001-20130704-avatar_wowzamonitor.sql
@@ -0,0 +1,3 @@
+﻿ALTER TABLE users ADD avatarsourceip TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER apiaddresses
+ALTER TABLE cdn_streaming_servers ADD serverstatus TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'reachable, unreachable' AFTER priority
+ALTER TABLE cdn_streaming_servers ADD currentload INT NULL DEFAULT NULL AFTER serverstatus
