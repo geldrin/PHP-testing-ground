e6fedef9371fbadaca868c9bb79022f1e4579e4f
Kovacs Andras <Andras.Kovacs@teleconnect.hu>
2013-07-08 14:22:32 +0200

    DB: streaming server monitoring password

--- resources/dbmigration/001-20130704-avatar_wowzamonitor.sql
+++ resources/dbmigration/001-20130704-avatar_wowzamonitor.sql
@@ -1,3 +1,4 @@
 ﻿ALTER TABLE users ADD avatarsourceip TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER apiaddresses;
 ALTER TABLE cdn_streaming_servers ADD serverstatus TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'reachable, unreachable' AFTER priority;
 ALTER TABLE cdn_streaming_servers ADD currentload INT NULL DEFAULT NULL AFTER serverstatus;
+ALTER TABLE cdn_streaming_servers ADD monitoringpassword TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER currentload;
