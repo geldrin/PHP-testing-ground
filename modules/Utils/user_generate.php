96da8033a794b7d619a05acabc6bb1e7d7ec81cf
Kovacs Andras <Andras.Kovacs@teleconnect.hu>
2013-08-22 15:41:59 +0200

    mediainfo masterlength fix #572

--- modules/Utils/user_generate.php
+++ modules/Utils/user_generate.php
@@ -19,7 +19,7 @@ $app = new Springboard\Application\Cli(BASE_PATH, PRODUCTION);
 // Organization ID = Conforg (ID: 200)
 $org_id = 200;
 // Department ID
-$org_dep_id = 62;
+$org_dep_id = 81;
 // How many users?
 $user_num = 50;
 // Single login: forced?
