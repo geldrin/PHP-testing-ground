190c0f7ac2fd6b3d3ba4d50e0597f3aa7c80cfb1
Peter SZTANOJEV <sztanpet@gmail.com>
2014-04-07 13:33:40 +0200

    [in progress: 711] Live intro: portál oldalon választható live intro, intro_servers is allitsunk

--- models/Livefeeds.php
+++ models/Livefeeds.php
@@ -390,6 +390,7 @@ class Livefeeds extends \Springboard\Model {
         )
       ;
 
+    $data['intro_servers'] = $data['livePlaceholder_servers'];
     $data['intro_streams'] = $data['livePlaceholder_streams'];
     return $data;
 
