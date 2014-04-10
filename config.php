d1873a330556a38932ec245b961a68500d0a205f
Peter SZTANOJEV <sztanpet@gmail.com>
2014-04-10 10:08:55 +0200

    framework bump es version bump

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $config = array(
-  'version'      => '_v201402312',
+  'version'      => '_v20140410',
   'charset'      => 'UTF-8',
   'cacheseconds' => 3600,
   'errormessage' => 'An unexpected error has occured, our staff has been notified. Sorry for the inconvenience and thanks for your understanding!',
