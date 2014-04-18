3a6d7b13767f7590dc2f9fd323ba371fe6919b1a
Kovacs Andras <Andras.Kovacs@teleconnect.hu>
2014-04-18 16:48:52 +0200

    [touch:125] HU help contentek initdb-be

--- data/defaultvalues/help_contents.php
+++ data/defaultvalues/help_contents.php
@@ -6,8 +6,119 @@ return array (
     'titleen' => 'Upload help',
     'shortname' => 'recordings_upload',
     'title_stringid' => 0,
-    'body' => '<p>A felvétel feltöltéséhez elsőként válassza ki a felvétel eredeti nyelvét!</p><p>Az <strong>"Állományok kiválasztása"</strong> gombra kattintva válassza ki a feltölteni kívánt felvételeket! Kérjük, figyelmesen olvassa el a <strong>"Felhasználási feltételek"</strong> oldalt, majd amennyiben elfogadja a tartalmát, hagyja jóvá a dokumentumot! A feltöltést a <strong>"Feltöltés kezdése"</strong> gomb megnyomásával indíthatja el.</p><p>Amennyiben rendelkezik <strong>HTML5</strong> böngészővel, használja a "fogd és vidd" (drag and drop) módszert! A fájlkezelő alkalmazásban válassza ki a feltölteni kívánt állományokat, majd dobja őket erre a lapra! Modern, HTML5 képes böngésző esetén egyszerre több állományt is feltölthet, valamint megszakadt feltöltéseit 1 héten át megőrizzük Önnek, így bármikor folytathatja azokat.</p>',
-    'bodyen' => '<p>To start upload a media file, select original language of the recording.</p><p>By clicking <strong>"Select files"</strong> select the recordings to be uploaded. Before starting the upload process, please read <strong>"Terms of Service"</strong> document and in case you agree with its content, approve it. To start upload, press <strong>"Start upload"</strong> button.</p><p>In case you have a <strong>HTML5</strong> enabled browser, use drag and drop technique to add files. In your file browser select media files and drop them onto this webpage. With a HTML5 enabled browser you can upload more files at a time. In addition, you can continue uploading your uncompleted recordings within a week from first starting the process.</p>',
+    'body' => '<p>Elsőként válassza ki a felvétel eredeti nyelvét!</p><p>Az <strong>"Állományok kiválasztása"</strong> gombra kattintva válassza ki a feltölteni kívánt média állomány(oka)t!</p><p>Kérjük, figyelmesen olvassa el a <strong>"Felhasználási feltételek"</strong> oldalt, majd amennyiben elfogadja tartalmát, hagyja jóvá a dokumentumot!</p><p>A feltölteni kívánt média fájlokat megjelölheti <strong>intro/outro</strong>ként. Az ilyen videókat a későbbiekben felvételekhez rendelheti, az intro a felvétel előtt, az outro pedig a felvétel után kerül majd automatikus lejátszásra.</p><p>A feltöltést a <strong>"Feltöltés kezdése"</strong> gomb megnyomásával indíthatja el.</p><p>Amennyiben rendelkezik <strong>HTML5</strong> böngészővel, használja a "fogd és vidd" (drag and drop) módszert! A fájlkezelő alkalmazásban válassza ki a feltölteni kívánt állományokat, majd dobja őket erre a lapra! Modern, HTML5 képes böngésző esetén egyszerre több állományt is feltölthet, valamint megszakadt feltöltéseit 1 héten át megőrizzük Önnek, így bármikor folytathatja azokat.</p>',
+    'bodyen' => '<p>To start upload a media file, select original language of the recording.</p><p>By clicking <strong>"Select files"</strong> select the media files to be uploaded.</p><p>Before starting the upload process, please read <strong>"Terms of Service"</strong> document and in case you agree with its content, approve it.</p><p>The media files to be uploaded can be marked as <strong>intro/outro</strong>. These videos can be associated with recordings later, intro is automatically played before, outro after the recording.</p><p>To start upload, press <strong>"Start upload"</strong> button.</p><p>In case you have a <strong>HTML5</strong> enabled browser, use drag and drop technique to add files. In your file browser select media files and drop them onto this webpage. With a HTML5 enabled browser you can upload more files at a time. In addition, you can continue uploading your uncompleted recordings within a week from first starting the process.</p>',
     'body_stringid' => 0,
   ),
+  1 => 
+  array (
+    'title' => 'Felvétel alapadatok',
+    'titleen' => 'Recording basics',
+    'shortname' => 'recordings_modifybasics',
+    'title_stringid' => 0,
+    'body' => '<p>A<strong>lapadatok</strong></p><p>Ezen a panelen a felvételre vonatkozó elemi információkat szerkesztheti.</p><p>A <strong>"Felvétel eredeti nyelve"</strong> listából válassza ki a felvétel nyelvét!</p><p>A <strong>"Cím"</strong> mezőben adja meg a felvétel címét (kötelező) és alcímét (opcionális)!</p><p>Amennyiben prezentációt is feltöltött a felvétel mellé, a <strong>"Prezentáció elhelyezkedése"</strong> opciónál (jobb/bal) tudja megadni a prezentáció alapértelmezett elhelyezését a lejátszóban.</p><p><strong>"Intro/Outro felvétel"</strong>: Lehetőséget biztosítunk a felvételek előtt intro (bevezető, főcím), illetve a felvételek után outro (végefőcím) automatikus lejátszására. Ehhez elsőként egy intro/outroként megjelölt videót kell feltöltenie (ld. <a href="../../hu/recordings/upload">"Feltöltés"</a> oldal). Ezt követően a legördülő listából válassza ki az aktuális felvételhez társítandó intro és/vagy outro részletet.</p><p>Végül válassza ki az alapértelmezett indexképet! A listákban, a csatornák tartalmában, keresési találatokban ez a kép reprezentálja majd a felvételt.</p><p>Az <strong>OK</strong> gombra kattintva elmentheti adatokat, majd a böngésző a következő panelre ugrik.</p>',
+    'bodyen' => '',
+    'body_stringid' => 0,
+  ),
+  2 => 
+  array (
+    'title' => 'Felhasználók meghívása',
+    'titleen' => 'Inviting new users',
+    'shortname' => 'users_invite',
+    'title_stringid' => 0,
+    'body' => '<p><strong>"E-mail cím</strong>": A meghívandó felhasználó e-mail címe. A mező kitöltése kötelező.</p><p><strong>"Jogosultságok":</strong> a jelölőnégyzetek segítségével  különböző jogokkal ruházhatja fel a meghívandó felhasználót (több jogosultság is választható):</p><ul><li><strong>Szerkesztő:</strong> Képes szerkeszteni/törölni a portálhoz tartozó összes felhasználó felvételeit, csatornákat hozhat létre, hírt vehet fel.</li><li><strong>Kliens adminsztátor:</strong> Portál adminisztrátor. Felhasználókat hívhat meg, jogosultságaikat kezelheti. Felhasználói csoportokat, illetve szervezeti egységeket hozhat létre. Szerkesztheti a kategória- és műfaj-rendszert.</li><li><strong>Feltöltő:</strong> Felvételeket tölthet fel a portálra, illetve szerkesztheti saját felvételeit.</li><li><strong>Live adminisztrátor:</strong> Élő közvetítéseket hozhat létre és menedzselhet, valamint moderálhatja a felhasználók hozzászólásait (chat).</li></ul><p>A <strong>"Szervezeti egységek"</strong> és a <strong>"Csoportok"</strong> menüponban a meghívandó felhasználót hozzárendelheti az előre létrehozott  szervezeti egységekhez vagy csoportokhoz, egyszerre több elem is választható. A felvételek,  illetve élő közvetítések elérhetőségét később, csoportok és szervezeti  egységek szerint korlátozhatja.</p><p>A <strong>"Felhasználó érvényességi idejének korlátozása"</strong> funkcióval a meghívandó felhasználó csak az itt megadott dátumig lesz képes a portálra bejelentkezni, ezt követően a fiók érvényességét veszti. Az <strong>igen</strong> opció kiválasztásakor megjelenő beviteli mezővel az érvényesség végdátuma adható meg. Az alsó gyorsválasztó segítségével havi, negyedéves, féléves és éves érvényességi időt jelölhet ki.</p>',
+    'bodyen' => '',
+    'body_stringid' => 0,
+  ),
+  3 => 
+  array (
+    'title' => 'Felvétel besorolása',
+    'titleen' => 'Classification',
+    'shortname' => 'recordings_modifyclassification',
+    'title_stringid' => 0,
+    'body' => '<p>Ebben a lépésben a videó műfaji, illetve tematikus kategorizálását végezhetjük el, a felvétel tartalmát kulcsszavak segítségével feltárhatjuk (keresés), valamint csatornához rendelhetjük.</p><p><strong>Kategóriák:</strong> A jelölőmezők segítségével az előre <a href="../../hu/categories/admin">létrehozott</a> kategóriákba sorolhatja a felvételt, így a tematikus <a href="../../hu/categories">kategória-rendszer</a> böngészésével a felhasználók könnyedén megtalálhatják. Egyszerre akár több kategória is választható, minimum egy kijelölése kötelező.</p><p><strong>Műfaj:</strong> Hasonlóan a kategóriákhoz, a felvételt az előre <a href="../../hu/genres/admin">definiált</a> műfajokhoz sorolhatja. Egyszerre több műfaj is választható, minimum egy kijelölése kötelező.</p><p><strong>Címkék:</strong> A címkék szabadon választott kulcsszavak, amelyek röviden leírják (feltárják) a felvétel tartalmát. A megfelelő kulcsszavakra keresve így megtalálhatjuk a felvételt. A szövegmezőben tetszőleges számú kifejezés megadható, amelyeket vesszővel (,) válasszon el egymástól!</p><p><strong>Felvétel hozzáadása csatornához:</strong> Amennyiben a felvételek tartalmilag kapcsolódnak, érdemes őket egy csatornához rendelni (pl. xyz konferencia), így az összetartozó anyagok egy helyen jelennek meg. A felvétel csatornához rendelése a csatorna mellett látható "+" jelre kattintással történik. Pipával jelöljük azokat a csatornákat, amelyekbe a felvételt már besorolta. A "-" jelre kattintva eltávolíthatja a felvételt a csatornából.</p><p>Az <strong>OK</strong> gombra kattintva a rendszer elmenti a változtatásokat és ugrik a következő lépésre.</p>',
+    'bodyen' => '',
+    'body_stringid' => 0,
+  ),
+  4 => 
+  array (
+    'title' => 'Felvétel leírása',
+    'titleen' => 'Recording description',
+    'shortname' => 'recordings_modifydescription',
+    'title_stringid' => 0,
+    'body' => '<p>Ezen az oldalon a felvétel néhány alapvető leíróját módosíthatja.</p><p><strong>Felvétel ideje:</strong> A felvétel elkészítésének ideje. A mezőre kattintva megjelenő panelen, vagy az értékeket közvetlen módosításával adható meg a megfelelő dátum és idő.</p><p><strong>Leírás:</strong> A felvétel tartalmának rövid leírása, amely a lejátszó alatt jelenik meg. Tartalma megjelenik a keresőben is, ezért érdemes kitölteni.</p><p><strong>Szerzői joggal kapcsolatos megjegyzés:</strong> A felvételre vonatkozó szerzői jogi nyilatkozatot itt lehet megadni, szintén a lejátszó alatt olvasható.</p><p><strong>Technikai megjegyzés:</strong> Saját jegyzet, csak a szerkesztő, illetve a feltöltő számára érhető el.</p><p>Az <strong>OK</strong> gombra kattintva a rendszer elmenti a változtatásokat és ugrik a következő lépésre.</p>',
+    'bodyen' => '',
+    'body_stringid' => 0,
+  ),
+  5 => 
+  array (
+    'title' => 'Felvétel közreműködők',
+    'titleen' => 'Recording contributors',
+    'shortname' => 'recordings_modifycontributors',
+    'title_stringid' => 0,
+    'body' => '<p>A Videosquare lehetőséget ad arra, hogy a felvételekhez közreműködőket (előadó, író, szerkesztő, stb.) rendeljen. A közreműködő személyt csak egyszer kell felvenni, ezt követően könnyedén hozzárendelheti más felvételekhez is.</p><p><strong>Közreműködő hozzáadása:</strong> Kezdje el gépelni a közreműködő nevét a szövegmezőben! A rendszer egy folyamatosan frissülő listában jeleníti meg a lehetséges személyeket.</p><p>Amennyiben az adatbázisban már létezik a megfelelő személy, válassza ki a listából és kattintson a <strong>"Hozzáadás"</strong> gombra. Ha nem találja a személyt, akkor az <strong>"Új közreműködő"</strong> segítségével adhat új szerzőt az adatbázishoz. Ehhez értelem szerűen töltse ki a megfelelő mezőket (keresztnév, vezetéknév, névsorrend, szerep), a jövőbeli azonosításhoz pedig rendeljen egy kulcsképet a szerzőhöz. Ebben a <strong>"Kép kiválasztása"</strong> funkció lesz segítségére, válasszon ki az aktuális felvételből egy, a személy azonosításra alkalmas képkockát!</p><p>A hozzáadott személyek listájában, az egyes nevek mellett néhány ikont láthat. A le/fel nyilakkal beállíthatja a közreműködők sorrendjét (a legfelső jelenik meg elsőként). A szemeteskosár ikonnal törölheti a személyt a felvétel közreműködői közül. A fogaskerék választásával szerkesztheti a személy adatait, a létrehozáskor megismert ablakkal fog találkozni.</p><p>Az <strong>OK</strong> gombra kattintva a rendszer elmenti a változtatásokat és ugrik a következő lépésre.</p>',
+    'bodyen' => '',
+    'body_stringid' => 0,
+  ),
+  6 => 
+  array (
+    'title' => 'Megosztás',
+    'titleen' => 'Share',
+    'shortname' => 'recordings_modifysharing',
+    'title_stringid' => 0,
+    'body' => '<p>Ebben a lépésben publikálhatja a felvételt, illetve beállíthatja a hozzáférési jogosultságokat. Négy különböző hozzáférési szint közül választhat:</p><p><strong>Publikus:</strong> A felvételt bárki megtekintheti.</p><p><strong>Regisztráció szükséges:</strong> Csak regisztrált felhasználók tekinthetik meg.</p><p><strong>Szervezeti egységek egy csoportja számára:</strong> Csak szervezeti egységek tagjai férnek hozzá.</p><p><strong>Saját csoportom számára:</strong> A megjelölt csoport tagjai férhetnek hozzá.</p><p>A továbbiakban néhány alapvető hozzáférési paramétert állíthat be:</p><p><strong>Korlátozza a hozzáférést dátum szerint?</strong> A felvétel csak a megadott időablakban érhető el. Adja meg a kezdő- és végdátumot!</p><p><strong>Eredeti felvétel letölthető?</strong> Letölthető legyen-e a felvétel a nézők számára? (nem javasolt)</p><p><strong>Audió sáv letölthető?</strong> Letölthető legyen-e a felvétel hangsávja? Hasznos lehet offline hallgatáshoz.</p><p><strong>Beágyazható?</strong> Amennyiben a felvételben olyan információkat találunk, amely adott kontextusban negatívan jelenhet meg, tilthatja a lejátszó külső weboldalakba történő beágyazását. A beágyazáshoz szükséges kódot a lejátszó alatt találja (ld. <strong><em>"&lt;&gt;"</em></strong> ikon).</p><p><strong>Felvétel állapota: </strong>A felvétel publikálásához az <em>"elérhető"</em> opciót kell választania. Alapértelmezett esetként a feltöltött videó <em>"vázlat"</em> állapotban van, azaz nem kerül automatikusan publikálásra. A vázlatokat kizárólag az adminisztrátorok és szerkesztők tekinthetik meg.</p><p><strong>Biztonsági szint:</strong> Lehallgatásra érzékeny tartalom esetén szükséges lehet a kliens és a Videosquare közötti médiafolyam titkosítása. Éles használatot megelőzően a tesztelés erősen ajánlott. <strong>VIGYÁZAT! Vegye figyelembe, hogy a mobil platformok egyelőre nem támogatják a média titkosítását!</strong></p><p><strong>Kreditpontos felvétel?</strong> A Videosquare rendszere lehetőséget ad a felvétel nézettségének nyomon követésére. Amennyiben ezt az opciót választja, a felvétel lejátszásakor csak a már lejátszott részben lehet pozícionálni. A nézettségről statisztika áll rendelkezésre.</p><p>Az <strong>OK </strong>gombra kattintva elmenti a beállításokat, és visszalép a felvételek listájára.</p>',
+    'bodyen' => '',
+    'body_stringid' => 0,
+  ),
+  7 => 
+  array (
+    'title' => 'Felirat feltöltése',
+    'titleen' => 'Upload subtitles',
+    'shortname' => 'recordings_uploadsubtitle',
+    'title_stringid' => 0,
+    'body' => '<p>Felvételeihez hagyományos <strong>.srt</strong> formátumú feliratfájlokat is feltölthet.</p><p>A felirat nyelvét a lenyíló menüből választhatja ki, az <em><strong>"Alapértelmezett felirat"</strong></em> engedélyezésével pedig az adott felirat automatikusan megjelenik a lejátszás elindításakor.</p><p><span style="font-family: Arial, sans-serif;">A felvételekhez egyszerre több feliratot is feltölthet, ezek a videó alatti listában fognak megjelenni és a </span><strong><em style="font-family: Arial, sans-serif;">"törlés"</em></strong> funkcióra kattintva törölhetőek</p>',
+    'bodyen' => '<p>You can upload <strong>.srt</strong> format subtitles to the recording.</p><p>Choose subtitle language from the dropdown list. By enabling <em><strong>"default subtitle"</strong></em> option this particular subtitle will be the default one and will be shown when playback is started.</p><p>More than one subtitle can be uploaded to each recording that will be listed under uploaded media items. Remove them any time by clicking "delete" option.</p>',
+    'body_stringid' => 0,
+  ),
+  8 => 
+  array (
+    'title' => 'Csatolmány feltöltése',
+    'titleen' => 'Uploading attachments',
+    'shortname' => 'recordings_uploadattachment',
+    'title_stringid' => 0,
+    'body' => '<p>A felvétele mellé bármilyen típusú- és számú dokumentumot feltölthet, amelyek a fenti listában jelennek meg. Kattintson a <em><strong>"tallózás"</strong></em> gombra, adja meg a fájl helyét, adjon egy címet a csatolmánynak, és kattintson az <em>"Ok"</em>-ra a feltöltés megkezdéséhez.</p><p>A csatolmány letöltő linkje csak akkor jelenik meg a felhasználók számára, ha a <strong><em>"Dokumentum elérhető"</em></strong> értéke <em>"igen"</em>-re van állítva.</p>',
+    'bodyen' => '',
+    'body_stringid' => 0,
+  ),
+  9 => 
+  array (
+    'title' => 'Prezentáció feltöltése',
+    'titleen' => 'Upload content video',
+    'shortname' => 'recordings_uploadcontent',
+    'title_stringid' => 0,
+    'body' => '<p>Az <strong>"Állomány kiválasztása"</strong> gombra kattintva válassza ki a feltölteni kívánt tartalom videót! Kérjük, figyelmesen olvassa el a <strong>"Felhasználási feltételek"</strong> oldalt, majd amennyiben elfogadja a tartalmát, hagyja jóvá a dokumentumot! A feltöltést a <strong>"Feltöltés kezdése"</strong> gomb megnyomásával indíthatja el.</p><p>Amennyiben rendelkezik <strong>HTML5</strong> böngészővel, használja a  "fogd és vidd" (drag and drop) módszert! A fájlkezelő alkalmazásban  válassza ki a feltölteni kívánt állományokat, majd dobja őket erre a  lapra! Modern, HTML5 képes böngésző esetén egyszerre több állományt is  feltölthet, valamint megszakadt feltöltéseit 1 héten át megőrizzük  Önnek, így bármikor folytathatja azokat.</p>',
+    'bodyen' => '',
+    'body_stringid' => 0,
+  ),
+  10 => 
+  array (
+    'title' => 'Csatornák létrehozása',
+    'titleen' => 'Creating channels',
+    'shortname' => 'channels_create',
+    'title_stringid' => 0,
+    'body' => '<p>Felvételeinek összerendezéséhez használjon csatornákat!</p><p>A csatorna létrehozásához adja meg a csatorna <strong><em>"Megnevezés"</em></strong>-ét, <em><strong>"Alcím"</strong></em>-ét, kedve szerint hozzáadhat egy rövid <em><strong>"Leírás"</strong></em>-t, végül pedig válassza ki az alsó lenyíló menüből a csatorna típusát!<br />Amennyiben nem szeretné, hogy a csatorna nyilvánosan hozzáférhető legyen, az alsó rádiógombok segítségével priváttá teheti.</p><p>Az <em><strong>"Ok"</strong></em> gombra kattintva létrehozhatja a csatornát, amelyet később a <em>"Főmenü &gt; Csatornáim"</em> menüpont alatt tekintheti meg, ahol később módosíthatja, illetve további alcsatornákat adhat hozzá.</p>',
+    'bodyen' => '',
+    'body_stringid' => 0,
+  ),
+  11 => 
+  array (
+    'title' => 'Élő adás létrehozása',
+    'titleen' => 'Create live event',
+    'shortname' => 'live_create',
+    'title_stringid' => 0,
+    'body' => '<p>Élő adás létrehozásához adja meg az közvetítésre vonatkozó alapinformációkat, úgy mint az esemény címét, alcímét, illetve leírását. A cím kitöltése kötelező. Válassza ki a lenyíló listából az <em><strong>"Esemény típu</strong><strong>sá"</strong></em>-t!</p><p>A <em><strong>"Kezdési-"</strong></em> és <em><strong>"Zárási időpont"</strong></em> beállításával meghatározhatja azt az időintervallumot, amelyen belül az élő esemény látható marad a portálon.</p><p>Az alsó rádiógombok segítségével a felhasználók hozzáférését szabályozhatja.</p>',
+    'bodyen' => '',
+    'body_stringid' => 0,
+  ),
+
 );
