d8781b763a12e72d734cad66bedaf216b06f3662
Peter SZTANOJEV <sztanpet@gmail.com>
2014-05-07 16:54:42 +0200

    [in progress: 877] Kreditpontos on demand nézések átalakítása

--- modules/Visitor/Recordings/Controller.php
+++ modules/Visitor/Recordings/Controller.php
@@ -1291,8 +1291,7 @@ class Controller extends \Visitor\Controller {
     if ( !$recordingsModel )
       throw new \Visitor\Api\ApiException('Recording not found', false, false );
     
-    $recordingsModel->updateLastPosition( $user['id'], $lastposition );
-    $recordingsModel->updateSession( $user['id'], session_id() );
+    $recordingsModel->updateLastPosition( $user['id'], $lastposition, session_id() );
     return true;
     
   }
