ad481298d90b7d39290be64dfd326c86e22f5811
Peter SZTANOJEV <sztanpet@gmail.com>
2014-05-07 18:00:35 +0200

    a teljes sessiont destroyoljuk hogy valtozzon a sessionid kilepeskor

--- modules/Visitor/Users/Controller.php
+++ modules/Visitor/Users/Controller.php
@@ -185,7 +185,8 @@ class Controller extends \Visitor\Controller {
     }
 
     $user->clear();
-    $this->redirectWithMessage('index', $l('users', 'loggedout') );
+    session_destroy();
+    $this->redirect('index');
     
   }
   
