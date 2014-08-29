ac0483ddfed10108d5828a957dcc2df6ac33e410
Peter SZTANOJEV <sztanpet@gmail.com>
2014-08-29 12:45:00 +0200

    [in progress: 1032] Content Delivery Network HTTP proxy alapon (CDN+), accesscheck.lua: whitespace tweak es apro bugfix

--- resources/server_config/edge/etc/nginx/accesscheck/accesscheck.lua
+++ resources/server_config/edge/etc/nginx/accesscheck/accesscheck.lua
@@ -1,12 +1,12 @@
 local recordingid = nil
 do
   -- block a lexikalis scope miatt, hogy ne leakeljen a matches foloslegesen
-  local regex = [[^/(?:devvsqlive|vsqlive|devvsq|vsq)/.*:\d+/(\d+)/\d+\..*\..*$]]
+  local regex = [[^/(?:devvsqlive|vsqlive|devvsq|vsq)/.*:\d+/(\d+)/\d+.*\..*$]]
   -- jo = regex options: jit, compile only once
   local matches, err = ngx.re.match( ngx.var.request_uri, regex, 'jo')
   if matches == nil then
     -- varatlan url, automatan engedjuk
-    ngx.log(ngx.DEBUG, ngx.var.request_uri, "unrecognized url, allow")
+    ngx.log(ngx.DEBUG, ngx.var.request_uri, " unrecognized url, allow")
     return ngx.exit(ngx.OK)
   end
 
@@ -16,7 +16,7 @@ end
 local sessionid = ngx.var.cookie_PHPSESSID
 if not sessionid or sessionid == '' then
   -- nincs sessionid, nincs mit ellenorizni
-  ngx.log(ngx.DEBUG, ngx.var.request_uri, "no sessionid, forbid")
+  ngx.log(ngx.DEBUG, ngx.var.request_uri, " no sessionid, forbid")
   return ngx.exit(ngx.HTTP_FORBIDDEN)
 end
 
@@ -27,13 +27,13 @@ local redis    = (require 'resty.redis'):new()
 redis:set_timeout(1000)
 local ok, err = redis:connect( ngx.var.redis_host, tonumber( ngx.var.redis_port ) )
 if not ok then
-  ngx.log(ngx.ERR, 'redis connect failed', err)
+  ngx.log(ngx.ERR, 'redis connect failed ', err)
   return ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
 end
 
 local ok, err redis:select( tonumber( ngx.var.redis_db ) )
 if not ok then
-  ngx.log(ngx.ERR, 'could not select redis db', err )
+  ngx.log(ngx.ERR, 'could not select redis db ', err )
   return ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
 end
 
@@ -49,7 +49,7 @@ if not result then
 
   local ok, err = redis:setex( cachekey, ngx.var.cacheexpirationsec, response.body )
   if not ok then
-    ngx.log(ngx.ERR, 'unable to set redis key, continuing', cachekey, err)
+    ngx.log(ngx.ERR, 'unable to set redis key, continuing ', cachekey, " err: " err)
   end
 
   result = response.body
@@ -63,7 +63,7 @@ else
   ret = ngx.HTTP_FORBIDDEN
 end
 
-ngx.log(ngx.DEBUG, ngx.var.request_uri, "result was", result)
+ngx.log(ngx.DEBUG, ngx.var.request_uri, " result was ", result)
 -- vissza a connection poolba, 10sec idle time-al, max 100 kapcsolattal
 redis:set_keepalive(10000, 100)
 ngx.exit(ret)
