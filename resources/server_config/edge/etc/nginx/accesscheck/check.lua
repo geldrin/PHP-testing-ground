d8ede95b2fc9c9b1dfdd54ee14f9f9e1ef52b297
Peter SZTANOJEV <sztanpet@gmail.com>
2014-09-01 14:49:39 +0200

    [in progress: 1032] Content Delivery Network HTTP proxy alapon (CDN+), proxy cacheles

--- resources/server_config/edge/etc/nginx/accesscheck/check.lua
+++ resources/server_config/edge/etc/nginx/accesscheck/check.lua
@@ -1,41 +1,8 @@
-local sessionid = nil
-do -- block a lexikalis scope miatt, hogy ne leakeljunk foloslegesen valtozokat
-  -- kizarolag HLS es HDS stream segmenseket validalunk
-  -- a query paramokat a rewrite strippelheti, ezert captureoljuk itt
-  -- a HDS-nel a parameterek base64 encodolva jonnek a filenevben, ezt is ki kell pucolni pl:
-  -- /devvsq/_definst_/mp4:105/105/105_295_video_360p.mp4/media_b306792_qY2xpZW50PTE=.abst/Seg1-Frag1
-  local regex = [[^/(?:devvsqlive|vsqlive|devvsq|vsq)/.*:\d+/\d+/\d+.*\..*/(?:media_\d+.ts|(?:_q([a-zA-Z0-9=+/])+\.abst/)?Seg[0-9]+-Frag[0-9]+.*).*?(?:\?(.*))?$]]
-  -- jo = regex options: jit, compile only once
-  local matches = ngx.re.match( ngx.var.request_uri, regex, 'jo')
-  if not matches then
-    -- varatlan url, automatan engedjuk
-    ngx.log(ngx.DEBUG, ngx.var.request_uri, " unrecognized url, allow")
-    -- nem HTTP_OK mert akkor elakad a request nalunk es nem megy tovabb a proxy-ra
-    return ngx.exit(ngx.OK)
-  end
-
-  if not matches[1] and not matches[2] then
-    -- nincs sessionid param, default nem engedjuk
-    ngx.log(ngx.DEBUG, ngx.var.request_uri, " no query/encoded params, forbid")
-    return ngx.exit(ngx.HTTP_FORBIDDEN)
-  end
-
-  local origargs  = ngx.var.args
-  if matches[1] then
-    ngx.var.args = ngx.decode_base64( matches[1] )
-  elseif matches[2] then
-    ngx.var.args = matches[2]
-  end
-
-  local arguments = ngx.req.get_uri_args(2)
-  if not arguments.sessionid then
-    -- nincs sessionid param, default nem engedjuk
-    ngx.log(ngx.DEBUG, ngx.var.request_uri, " no sessionid in query params, forbid")
-    return ngx.exit(ngx.HTTP_FORBIDDEN)
-  end
-
-  sessionid    = arguments.sessionid
-  ngx.var.args = origargs
+local sessionid = ngx.var.sessionid
+if not ngx.re.find( sessionid, [[^\d+_[a-z0-9]+_\d+$]], 'jo') then
+  -- nem megfelelo sessionid param, default nem engedjuk
+  ngx.log(ngx.DEBUG, ngx.var.request_uri, ' sessionid invalid, forbid: ', sessionid )
+  return ngx.exit(ngx.HTTP_FORBIDDEN)
 end
 
 local cachekey = sessionid
@@ -59,6 +26,7 @@ if not result or result == ngx.null then
   -- nincs cacheben, meghivjuk es eltesszuk
   -- ezek internal location-ok mert nem tudunk tetszes szerinti urlt meghivni magunk
   local checkuri = ngx.var.scheme == 'https' and "/secureaccesscheck" or "/accesscheck"
+  ngx.log(ngx.DEBUG, 'calling url to check: ', checkuri, ' with param sessionid=', sessionid )
   local response = ngx.location.capture( checkuri, {['args'] = {['sessionid'] = sessionid}} )
 
   if response.status ~= ngx.HTTP_OK then
--- resources/server_config/edge/etc/nginx/accesscheck/rewrite.lua
+++ resources/server_config/edge/etc/nginx/accesscheck/rewrite.lua
@@ -1,12 +1,35 @@
--- a parametereket strippeljuk a cachelhetoseg kedveert
-ngx.req.set_uri_args('')
+-- a parametereket strippeljuk a cachelhetoseg kedveert mindig
+do
+  local arguments = ngx.req.get_uri_args(2)
+  if arguments.sessionid then
+    ngx.var.sessionid = arguments.sessionid
+    ngx.log(ngx.DEBUG, ngx.var.request_uri, ' sessionid param found, OK')
+    ngx.req.set_uri_args('')
+    return ngx.exit(ngx.OK)
+  end
+end
 
 -- a HDS-nel a parameterek base64 encodolva jonnek a filenevben, ezt is ki kell pucolni
--- itt peldaul a "client=1" param van atadva, a _q a prefix
+-- itt peldaul a 'client=1' param van atadva, a _q a prefix
 -- /devvsq/_definst_/mp4:105/105/105_295_video_360p.mp4/media_b306792_qY2xpZW50PTE=.abst/Seg1-Frag1
-local regex  = [[^(.*)(?:_q[a-zA-Z0-9=+/]+)(\.abst/Seg[0-9]+-Frag[0-9]+.*)$]]
-local newuri = ngx.re.sub( ngx.var.request_uri, regex, 'jo')
-if newuri then
-  ngx.log(ngx.DEBUG, "rewritten url from: ", ngx.var.request_uri, " to: ", newuri )
-  ngx.req.set_uri( newuri )
+local regex   = [[^(.+)(?:_q([a-zA-Z0-9=+/]+))(\.abst/Seg[0-9]+-Frag[0-9]+)$]]
+local matches = ngx.re.match( ngx.var.request_uri, regex, 'jo')
+if not matches then
+  ngx.log(ngx.DEBUG, ngx.var.request_uri, ' unknown url, forbid')
+  return ngx.exit(ngx.HTTP_FORBIDDEN)
 end
+
+ngx.var.args    = ngx.decode_base64( matches[2] )
+local arguments = ngx.req.get_uri_args(2)
+if not arguments.sessionid then
+  -- nincs sessionid param, default nem engedjuk
+  ngx.log(ngx.DEBUG, ngx.var.request_uri, ' no sessionid in encoded filename found, forbid')
+  return ngx.exit(ngx.HTTP_FORBIDDEN)
+end
+ngx.var.sessionid = arguments.sessionid
+ngx.req.set_uri_args('')
+
+local newuri = matches[1] .. matches[3]
+ngx.log(ngx.DEBUG, 'rewritten url from: ', ngx.var.request_uri, ' to: ', newuri )
+ngx.req.set_uri( newuri )
+ngx.var.cachekey = cachekey
