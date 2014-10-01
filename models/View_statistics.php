6eb29e7eec156ab0f33257066ea052d1174e31f2
Peter SZTANOJEV <sztanpet@gmail.com>
2014-10-01 16:07:18 +0200

    [in progress: 1031] Statisztika reporting HTTP API hívás

--- models/View_statistics.php
+++ models/View_statistics.php
@@ -1,8 +1,18 @@
 <?php
 namespace Model;
 
-class View_statistics extends \Springboard\Model {
-  
+abstract class View_statistics extends \Springboard\Model {
+  protected static $updateKeepFields = array();
+  protected static $stateTable = array(
+    'PLAY'    => 'newSlice',
+    'SEEK'    => 'closeAndCreateSlice',
+    'PAUSE'   => 'closeSlice',
+    'STOP'    => 'closeSlice',
+    'PLAYING' => 'updateSlice',
+  );
+
+  abstract public function log( $values );
+
   public function populateStreamInfo( &$values ) {
     if ( !isset( $values['url'] ) )
       return $values;
@@ -15,6 +25,62 @@ class View_statistics extends \Springboard\Model {
     $values['streamurl']    = $streamdata['path'];
 
     return $values;
+  }
+
+  protected function runStateMachine( $values ) {
+    $this->startTrans();
+
+    if ( !isset( static::$stateTable[ $values['action'] ] ) )
+      throw new \Exception("Unhandled action: " . $values['action'] );
+
+    $method = static::$stateTable[ $values['action'] ];
+    $this->$method( $values );
+
+    $this->endTrans();
+  }
+
+  protected function newSlice( $values ) {
+    $values['startaction'] = $values['action'];
+    $this->insert( $values );
+  }
+
+  protected function closeAndCreateSlice( $values ) {
+    $this->closeSlice( $values );
+    $this->newSlice( $values );
+  }
+
+  protected function closeSlice( $values ) {
+    $this->updateSlice( $values, $values['action'] );
+  }
+
+  protected function updateSlice( $values, $stopaction = null ) {
+    $viewid   = $this->db->qstr( $values['viewsessionid'] );
+    $this->id = $this->db->getOne("
+      SELECT id
+      FROM view_statistics_ondemand
+      WHERE viewsessionid = $viewid
+      ORDER BY id DESC
+      LIMIT 1
+    ");
+
+    if ( !$this->id )
+      throw new \Exception("No open slice found!");
+
+    $filteredvalues = array();
+    foreach( $values as $field => $value ) {
+      if ( isset( static::$updateKeepFields[ $field ] ) )
+        $filteredvalues[ $field ] = $value;
+    }
+
+    if ( $stopaction )
+      $filteredvalues['stopaction'] = $stopaction;
+
+    if ( empty( $filteredvalues ) )
+      throw new \Exception(
+        "Nothing to update the open slice with: " . var_export( $values, true )
+      );
+
+    $this->updateRow( $filteredvalues );
   }
 
 }
--- models/View_statistics_live.php
+++ models/View_statistics_live.php
@@ -2,35 +2,22 @@
 namespace Model;
 
 class View_statistics_live extends \Model\View_statistics {
-  
+  protected static $updateKeepFields = array(
+    'timestampuntil' => true,
+  );
+
   public function log( $values ) {
-    $values              = $this->populateStreamInfo( $values );
+    $values = $this->populateStreamInfo( $values );
     $values['timestampuntil'] = date('Y-m-d H:i:s');
 
-    if ( $values['action'] == 'PLAYING' ) {
+    $this->runStateMachine( $values );
 
-      $fields = array();
-      foreach( $values as $field => $value )
-        $fields []= "$field = " . $this->db->qstr( $value );
+  }
 
-      $this->db->execute("
-        UPDATE view_statistics_ondemand
-        SET " . implode(", ", $fields ) . "
-        WHERE
-          viewsessionid = " . $this->db->qstr( $values['viewsessionid'] ) . " AND
-          action        = '" . $values['action'] . "'
-      ");
-
-      if ( $this->db->affected_rows()  <= 0 ) {
-        $values['timestampfrom'] = $values['timestampuntil'];
-        $this->insert( $values );
-      }
-
-    } else {
-      $values['timestampfrom'] = $values['timestampuntil'];
-      $this->insert( $values );
-    }
-
+  protected function newSlice( $values ) {
+    unset( $values['timestampuntil'] );
+    $values['timestampfrom'] = date('Y-m-d H:i:s');
+    parent::newSlice( $values );
   }
 
 }
