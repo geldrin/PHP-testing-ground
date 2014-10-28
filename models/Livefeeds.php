2722e63fea2562f2f5642f572466af4ece91050c
Peter SZTANOJEV <sztanpet@gmail.com>
2014-10-28 12:20:22 +0100

    [in progress: 1150] Flash player: nincs címkeátadás élő adásnál

--- models/Livefeeds.php
+++ models/Livefeeds.php
@@ -246,7 +246,36 @@ class Livefeeds extends \Springboard\Model {
     
   }
   
-  public function getStreamUrls( $info, $prefix = '' ) {
+  private function getStreamInfo( $info, $prefix = '' ) {
+
+    $ret = array(
+      'streams'    => array(),
+      'labels'     => array(
+        mb_substr( $info['streams']['defaultstream']['name'], 0, 6 ),
+      ),
+      'parameters' => array(
+        array(
+          'viewsessionid' => $this->generateViewSessionid(
+            $info['streams']['defaultstream']['id']
+          ),
+        ),
+      ),
+    );
+
+    foreach( $info['streams']['streams'] as $stream ) {
+
+      if (
+           $info['streams']['defaultstream']['id'] == $stream['id'] or
+           $info['streams']['defaultstream']['quality'] == $stream['quality']
+         )
+        continue;
+
+      $ret['labels'][]     = mb_substr( $stream['name'], 0, 6 );
+      $ret['parameters'][] = array(
+        'viewsessionid' => $this->generateViewSessionid( $stream['id'] ),
+      );
+
+    }
 
     if ( $this->isHDSEnabled( $prefix ) ) {
 
@@ -257,24 +286,11 @@ class Livefeeds extends \Springboard\Model {
       if ( $prefix )
         $filename .= '_' . $prefix;
 
-      if ( $authorizecode )
-        $authorizecode .= '&';
-      else
-        $authorizecode .= '?';
-
-      $authorizecode .= 'viewsessionid=' . $this->generateViewSessionid(
-        $authorizecode
-      );
-      $streams = array(
-        sprintf( $smilurl, $filename, $authorizecode )
-      );
-
-      return $streams;
+      $ret['streams'][] = sprintf( $smilurl, $filename, $authorizecode );
 
     } else {
 
-      $streams   = array();
-      $streams[] = $info['streams']['defaultstream'][ $prefix . 'keycode'];
+      $ret['streams'][] = $info['streams']['defaultstream'][ $prefix . 'keycode'];
 
       foreach( $info['streams']['streams'] as $stream ) {
 
@@ -284,18 +300,13 @@ class Livefeeds extends \Springboard\Model {
            )
           continue;
 
-        $streams[] = $stream[ $prefix . 'keycode'];
+        $ret['streams'][] = $stream[ $prefix . 'keycode'];
 
       }
 
-      foreach( $streams as $key => $value )
-        $streams[ $key ] .= '?viewsessionid=' . $this->generateViewSessionid(
-          $value
-        );
-
-      return $streams;
-
     }
+
+    return $ret;
 
   }
 
@@ -314,13 +325,19 @@ class Livefeeds extends \Springboard\Model {
       'feed_id'                => $this->id,
       'recording_title'        => $this->row['name'],
       'recording_type'         => 'live',
-      'media_streams'          => $this->getStreamUrls( $info ),
-      'media_secondaryStreams' => $this->getStreamUrls( $info, 'content'),
       'timeline_autoPlay'      => true,
       'user_checkWatching'     => (bool)$info['member']['ispresencecheckforced'],
       'user_checkWatchingTimeInterval' => $info['checkwatchingtimeinterval'],
       'user_checkWatchingConfirmationTimeout' => $info['checkwatchingconfirmationtimeout'],
     );
+
+    $streaminfo = $this->getStreamInfo( $info );
+    $flashdata['media_streams']          = $streaminfo['streams'];
+    $flashdata['media_streamLabels']     = $streaminfo['labels'];
+    $flashdata['media_streamParameters'] = $streaminfo['parameters'];
+
+    $streaminfo = $this->getStreamInfo( $info, 'content');
+    $flashdata['media_secondaryStreams'] = $streaminfo['streams'];
 
     if ( $info['member'] and $info['member']['id'] ) {
       $flashdata['user_id']          = $info['member']['id'];
