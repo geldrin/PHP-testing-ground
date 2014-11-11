15f92ac90c71d05a6fc5525a35c6aab71952fffa
VSQ Conv <andras.kovacs@teleconnect.hu>
2014-11-11 18:44:13 +0100

    [touch:1190] job stats2

--- modules/Jobs/job_stats_process2.php
+++ modules/Jobs/job_stats_process2.php
@@ -20,7 +20,7 @@ $app = new Springboard\Application\Cli(BASE_PATH, PRODUCTION);
 // Load jobs configuration file
 $app->loadConfig('modules/Jobs/config_jobs.php');
 $jconf = $app->config['config_jobs'];
-$myjobid = $jconf['jobid_stats_process'];
+$myjobid = $jconf['jobid_stats_process'] . "2";
 $debug = Springboard\Debug::getInstance();
 
 // !!!
@@ -101,7 +101,7 @@ $platform_definitions = array(
 // Flash IDE Builder: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/537.75.14
 );
 
-$vsq_epoch = strtotime("2012-11-30 23:59:59");
+$vsq_epoch = strtotime("2014-11-01 00:00:00");
 
 $stats_config = array(
   0 => array(
@@ -144,7 +144,7 @@ $db = db_maintain();
 // Check Wowza records with open endtime
 $now_hour = date("G");
 $now_min = date("i");
-if ( ( $now_hour == 11 ) and ($now_min > 0) and ($now_min < 5 ) ) checkWowzaOpenRecords();
+//if ( ( $now_hour == 11 ) and ($now_min > 0) and ($now_min < 5 ) ) checkWowzaOpenRecords();
 
 // Empty array for each record initialization
 $platforms_null = returnStreamingClientPlatformEmptyArray($platform_definitions);
@@ -242,7 +242,7 @@ for ( $statsidx = 0; $statsidx < count($stats_config); $statsidx++ ) {
 
       // Next record breaking the inactive period that also occured before the end of the last whole interval
       $now_left = getTimelineGridSeconds(time(), "left", $stats_config[$statsidx]['interval']);
-      $next_active_record = getFirstWowzaRecordFromInterval($end_interval, $now_left, $wowza_app);
+      $next_active_record = getFirstLiveStatRecordFromInterval($end_interval, $now_left, $wowza_app);
       if ( $next_active_record === false ) {
         // Nothing to process, cdn_streaming_stats DB is empty from end of interval. Exit processing.
 
@@ -270,7 +270,7 @@ for ( $statsidx = 0; $statsidx < count($stats_config); $statsidx++ ) {
       // Query finally selected interval with record(s)
       $stats = queryStatsForInterval($start_interval, $end_interval, $wowza_app);
       if ( $stats === false ) {
-        $debug->log($jconf['log_dir'], $myjobid . ".log", "[WARN] Unexpected. No record(s) to process. Interval: " . date("Y-m-d H:i:s", $start_interval) . " - " . date("Y-m-d H:i:s", $end_interval) . "\n\nDEBUG:\n\ngetFirstWowzaRecordFromInterval(): " . $kaka2 . "\n\nqueryStatsForInterval(): " . $kaka, $sendmail = false);
+        $debug->log($jconf['log_dir'], $myjobid . ".log", "[WARN] Unexpected. No record(s) to process. Interval: " . date("Y-m-d H:i:s", $start_interval) . " - " . date("Y-m-d H:i:s", $end_interval) . "\n\nDEBUG:\n\ngetFirstLiveStatRecordFromInterval(): " . $kaka2 . "\n\nqueryStatsForInterval(): " . $kaka, $sendmail = false);
         // Update last processed interval. Next active record will be found in next round.
         $stats_config[$statsidx]['lastprocessedtime'] = $end_interval;
         $start_interval = $start_interval + $stats_config[$statsidx]['interval'];
@@ -288,72 +288,61 @@ for ( $statsidx = 0; $statsidx < count($stats_config); $statsidx++ ) {
 
     while ( !$stats->EOF ) {
 
-      $stat = $stats->fields;
-//echo "DEBUG: Wowza record\n";
-//var_dump($stat);
+        $stat = $stats->fields;
 
-      // Live feed ID
+        // Live feed ID
+        if ( !empty($stat['livefeedid']) ) {
+            $feedid = $stat['livefeedid'];
+        } else {
+            $debug->log($jconf['log_dir'], $myjobid . ".log", "[WARN] No livefeedid for stats#" . $stat['id'] . ". Skipping.", $sendmail = false);
+            $stats->MoveNext();
+            continue;
+        }
 
-      // Normal client: livefeedid comes from Wowza URL (logged in DB)
-      if ( is_numeric($stat['vsqrecordingfeed']) ) {
-        $feedid = $stat['vsqrecordingfeed'];
-      } else {
-        // If null, then get it from livefeed_streams
-        $feedid = $stat['livefeedid'];
-      }
+        $streamid = $stat['livefeedstreamid'];          // livefeeds_stream.id
+        $qualitytag = $stat['streamname'];              // SD, HD, etc.
+        $streamname = $stat['keycode'];                 // Wowza stream name (e.g. 123456)
+        $contentstreamname = $stat['contentkeycode'];   // Wowza content stream name
 
-      $streamid = $stat['streamid'];    // livefeeds_stream.id
-      $qualitytag = $stat['qualitytag'];  // SD, HD, etc.
-      $streamname = $stat['keycode'];   // Wowza stream name (e.g. 123456)
-      $contentstreamname = $stat['contentkeycode'];
+        // User agent
+        $platform = findStreamingClientPlatform($stat['useragent']);
 
-      $platform = findStreamingClientPlatform($stat['clientplayer']);
+        // Country (geo IP)
+        $country = geoip_country_code_by_name($stat['ipaddress']);
+        if ( $country === false ) {
+            $country = "notdef";
+        }
 
-      // Country (geo IP)
-      $country = geoip_country_code_by_name($stat['clientip']);
-      if ( $country === false ) {
-        $country = "notdef";
-      }
-
-      // Is content?
-      $iscontent = 0;
-      if ( $stat['wowzalocalstreamname'] == $stat['contentkeycode'] ) {
-        $iscontent = 1;
-      } elseif ( $stat['wowzalocalstreamname'] != $stat['keycode'] ) {
-        $debug->log($jconf['log_dir'], $myjobid . ".log", "[WARN] No video/content match. Wowza says: " . $stat['wowzalocalstreamname'] . " / db keycode: " . $stat['keycode'] . "(record id: " . $stat['id'] . ")", $sendmail = false);
-      }
-
-      //// Statistics records filtered
-      // Build array index
-      $idx = $streamid . "_" . $iscontent . "_" . $country; 
-      // Initialize (if not yet record is open)
-      if ( !isset($stats_f[$feedid][$idx]) ) {
-        $stats_f[$feedid][$idx] = $platforms_null;
-        $stats_f[$feedid][$idx]['timestamp'] = date("Y-m-d H:i:s", $start_interval);
-        $stats_f[$feedid][$idx]['iscontent'] = $iscontent;
-      }
-      // Add (repeat) livefeedid
-      if ( !isset($stats_f[$feedid][$idx]['livefeedid']) ) {
-        $stats_f[$feedid][$idx]['livefeedid'] = $feedid;
-      }
-      // Add livefeedstreamid
-      if ( !isset($stats_f[$feedid][$idx]['livefeedstreamid']) ) {
-        $stats_f[$feedid][$idx]['livefeedstreamid'] = $streamid;
-      }
-      // Add country
-      if ( !isset($stats_f[$feedid][$idx]['country']) ) {
-        $stats_f[$feedid][$idx]['country'] = $country;
-      }
-      // Increase platform counter
-      if ( isset($stats_f[$feedid][$idx][$platform]) ) {
-        $stats_f[$feedid][$idx][$platform]++;
-      }
+        //// Statistics records filtered
+        // Build array index
+        $idx = $streamid . "_" . $country; 
+        // Initialize (if not yet record is open)
+        if ( !isset($stats_f[$feedid][$idx]) ) {
+            $stats_f[$feedid][$idx] = $platforms_null;
+            $stats_f[$feedid][$idx]['timestamp'] = date("Y-m-d H:i:s", $start_interval);
+        }
+        // Add (repeat) livefeedid
+        if ( !isset($stats_f[$feedid][$idx]['livefeedid']) ) {
+            $stats_f[$feedid][$idx]['livefeedid'] = $feedid;
+        }
+        // Add livefeedstreamid
+        if ( !isset($stats_f[$feedid][$idx]['livefeedstreamid']) ) {
+            $stats_f[$feedid][$idx]['livefeedstreamid'] = $streamid;
+        }
+        // Add country
+        if ( !isset($stats_f[$feedid][$idx]['country']) ) {
+            $stats_f[$feedid][$idx]['country'] = $country;
+        }
+        // Increase platform counter
+        if ( isset($stats_f[$feedid][$idx][$platform]) ) {
+            $stats_f[$feedid][$idx][$platform]++;
+        }
 
 //echo "DEBUG: processed record\n";
 //var_dump($stats_f);
 
-      $records_processed++;
-      $stats->MoveNext();
+        $records_processed++;
+        $stats->MoveNext();
     } // End of stats while loop
 
     // Update results to DB
@@ -361,15 +350,14 @@ for ( $statsidx = 0; $statsidx < count($stats_config); $statsidx++ ) {
     $records_committed = 0;
     if ( count($stats_f) > 0 ) {
 
-      $records_committed = 0;
-//echo "DB:\n";
-      foreach ( $stats_f as $feedid => $feed_array ) {
-        foreach ( $feed_array as $idx => $stat_record ) {
-          insertStatRecord($stat_record, $stats_config[$statsidx]['sqltablename']);
-//var_dump($stat_record);
-          $records_committed++;
+        $records_committed = 0;
+        foreach ( $stats_f as $feedid => $feed_array ) {
+            foreach ( $feed_array as $idx => $stat_record ) {
+                insertStatRecord($stat_record, $stats_config[$statsidx]['sqltablename']);
+                //var_dump($stat_record);
+                $records_committed++;
+            }
         }
-      }
 
     }
 
@@ -425,58 +413,42 @@ global $db, $debug, $myjobid, $app, $jconf, $kaka;
 
   $query = "
     SELECT
-      css.id,
-      css.vsqsessionid,
-      css.vsqdomain,
-      css.vsqrecordingfeed,
-      css.wowzasessionid,
-      css.starttime,
-      css.endtime,
-      css.vsquserid,
-      css.httpsessionid,
-      css.wowzaappid,
-      css.sessiontype,
-      css.wowzalocalstreamname,
-      css.wowzaremotestreamname,
-      css.wowzaclientid,
-      css.rtspsessionid,
-      css.streamingtype,
-      css.serverip,
-      css.clientip,
-      css.encoder,
-      css.url,
-      css.referrer,
-      css.clientplayer,
-      lfs.id AS streamid,
-      lfs.livefeedid,
-      lfs.name AS qualitytag,
-      lfs.keycode,
-      lfs.contentkeycode
+        vsl.id,
+        vsl.userid,
+        vsl.livefeedid,
+        vsl.livefeedstreamid,
+        vsl.sessionid,
+        vsl.viewsessionid,
+        vsl.startaction,
+        vsl.stopaction,
+        vsl.streamscheme,
+        vsl.streamserver,
+        vsl.streamurl,
+        vsl.ipaddress,
+        vsl.useragent,
+        vsl.timestampfrom,
+        vsl.timestampuntil,
+        lf.name AS feedname,
+        lfs.name AS streamname,
+        lfs.keycode,
+        lfs.contentkeycode  
     FROM
-      cdn_streaming_stats AS css,
-      livefeed_streams AS lfs,
-      converter_nodes AS cn
+        view_statistics_live as vsl,
+        livefeeds AS lf,
+        livefeed_streams AS lfs
     WHERE
-      css.wowzaappid = '" . $streaming_server_app . "' AND
-      ( css.streamingtype = 'http' OR css.streamingtype = 'rtsp' ) AND
-      css.clientip <> cn.serverip AND (
-      ( css.starttime >= '" . $start_interval_datetime . "' AND css.starttime <= '" . $end_interval_datetime . "' ) OR  # START in the interval
-      ( css.endtime >= '" . $start_interval_datetime . "'   AND css.endtime <= '" . $end_interval_datetime . "' ) OR    # END in the interval
-      ( css.starttime <= '" . $end_interval_datetime . "' AND css.endtime IS NULL ) OR                  # Open record
-      ( css.starttime < '" . $start_interval_datetime . "' AND css.endtime > '" . $end_interval_datetime . "' ) ) AND   # Record covering the whole interval
-      ( ( css.vsqrecordingfeed IS NOT NULL AND css.vsqrecordingfeed = lfs.livefeedid ) OR ( css.vsqrecordingfeed IS NULL AND css.vsqsessionid IS NULL) )
-      AND ( css.wowzalocalstreamname = lfs.keycode OR css.wowzalocalstreamname = lfs.contentkeycode )
-    GROUP BY
-      css.vsqsessionid, css.wowzalocalstreamname, css.vsqrecordingfeed";
-
-// Ez a VCR-es izeket kicsinálja!!!! keycode változik!
-//AND ( css.wowzalocalstreamname = lfs.keycode OR css.wowzalocalstreamname = lfs.contentkeycode )
+        vsl.streamurl LIKE '/" . $streaming_server_app . "%'
+        AND (
+        ( vsl.timestampfrom >= '" . $start_interval_datetime . "' AND vsl.timestampfrom <= '" . $end_interval_datetime . "' ) OR
+        ( vsl.timestampuntil >= '" . $start_interval_datetime . "' AND vsl.timestampuntil <= '" . $end_interval_datetime . "' ) OR
+        ( vsl.timestampfrom < '" . $start_interval_datetime . "' AND vsl.timestampuntil > '" . $end_interval_datetime . "' ) )
+        AND vsl.livefeedid = lf.id
+        AND vsl.livefeedstreamid = lfs.id
+    ";
 
 //echo $query . "\n";
-$kaka = $query;
-// Record problémák:
-// - minőségi váltás: nincs új rekord (Wowza oldalon nem figyelünk valami eventet?)
-// - nem lezáruló rekordok: Wowza restart?
+
+  $kaka = $query;
 
   try {
     $stats = $db->Execute($query);
@@ -493,7 +465,7 @@ $kaka = $query;
 
 // Get next live statistics record timestamp (to help jumping empty time slots)
 // WARNING: does not check for active records
-function getFirstWowzaRecordFromInterval($from_timestamp, $to_timestamp, $streaming_server_app) {
+function getFirstLiveStatRecordFromInterval($from_timestamp, $to_timestamp, $streaming_server_app) {
  global $db, $debug, $myjobid, $app, $jconf, $kaka2;
 
   if ( empty($from_timestamp) ) $from_timestamp = 0;
@@ -503,25 +475,23 @@ function getFirstWowzaRecordFromInterval($from_timestamp, $to_timestamp, $stream
 
   $query = "
     SELECT
-      css.id,
-      css.starttime
+        vsl.id,
+        vsl.timestampfrom
     FROM
-      cdn_streaming_stats AS css,
-      converter_nodes AS cn
+        view_statistics_live as vsl
     WHERE
-      css.clientip <> cn.serverip AND
-      css.starttime >= '" . $from_datetime . "' AND
-      css.starttime <= '" . $to_datetime . "' AND
-      css.wowzaappid = '" . $streaming_server_app . "' AND
-      css.wowzalocalstreamname IS NOT NULL AND
-      css.wowzalocalstreamname <> ''
+        vsl.timestampfrom >= '" . $from_datetime . "' AND
+        vsl.timestampfrom <= '" . $to_datetime . "' AND
+        vsl.streamurl LIKE '/" . $streaming_server_app . "%'
     ORDER BY
-      css.starttime
-    LIMIT 1";
+        vsl.timestampfrom
+    LIMIT 1
+    ";
 
 //echo $query . "\n";
-$kaka2 = $query;
 
+  $kaka2 = $query;
+  
   try {
     $stats = $db->getArray($query);
   } catch (exception $err) {
@@ -532,7 +502,7 @@ $kaka2 = $query;
   // No records in database
   if ( count($stats) < 1 ) return false;
 
-  $starttime = strtotime($stats[0]['starttime']);
+  $starttime = strtotime($stats[0]['timestampfrom']);
 
   return $starttime;
 }
@@ -624,18 +594,18 @@ function insertStatRecord($stat_record, $db_stats_table) {
 global $debug, $app, $jconf, $myjobid;
 
   $values = array(
-    'timestamp'       => $stat_record['timestamp'],
-    'livefeedid'      => $stat_record['livefeedid'],
-    'livefeedstreamid'    => $stat_record['livefeedstreamid'],
-    'iscontent'       => $stat_record['iscontent'],
-    'country'       => $stat_record['country'],
-    'numberofflashwin'    => $stat_record['flashwin'],
-    'numberofflashmac'    => $stat_record['flashmac'],
-    'numberofflashlinux'  => $stat_record['flashlinux'],
-    'numberofandroid'   => $stat_record['android'],
-    'numberofiphone'    => $stat_record['iphone'],
-    'numberofipad'      => $stat_record['ipad'],
-    'numberofunknown'   => $stat_record['unknown'],
+    'timestamp'             => $stat_record['timestamp'],
+    'livefeedid'            => $stat_record['livefeedid'],
+    'livefeedstreamid'      => $stat_record['livefeedstreamid'],
+    'iscontent'             => 0,
+    'country'               => $stat_record['country'],
+    'numberofflashwin'      => $stat_record['flashwin'],
+    'numberofflashmac'      => $stat_record['flashmac'],
+    'numberofflashlinux'    => $stat_record['flashlinux'],
+    'numberofandroid'       => $stat_record['android'],
+    'numberofiphone'        => $stat_record['iphone'],
+    'numberofipad'          => $stat_record['ipad'],
+    'numberofunknown'       => $stat_record['unknown'],
   );
 
 //var_dump($values);
@@ -655,63 +625,12 @@ function recursive_array_search($needle,$haystack) {
     
   foreach( $haystack as $key=>$value ) {
         $current_key = $key;
-        if ( $needle === $value OR ( is_array($value) && recursive_array_search($needle,$value) !== false ) ) {
+        if ( $needle === $value OR ( is_array($value) && recursive_array_search($needle, $value) !== false ) ) {
             return $current_key;
         }
     }
 
     return false;
-}
-
-function fixkaka() {
-global $db, $debug, $myjobid, $app, $jconf;
-
-  $query = "
-    SELECT
-      id,
-      starttime,
-      endtime
-    FROM
-      cdn_streaming_stats
-    WHERE
-      endtime IS NULL
-    ";
-
-//echo $query . "\n";
-
-  try {
-    $stats = $db->getArray($query);
-  } catch (exception $err) {
-    $debug->log($jconf['log_dir'], $myjobid . ".log", "[ERROR] SQL query failed." . trim($query), $sendmail = false);
-    return false;
-  }
-
-  for ( $i = 0; $i < count($stats); $i++ ) {
-    var_dump($stats[$i]);
-    $time = strtotime($stats[$i]['starttime']) + 60;
-    $dtime = date("Y-m-d H:i:s", $time);
-
-    $query = "
-      UPDATE
-        cdn_streaming_stats
-      SET
-        endtime = '" . $dtime . "'
-      WHERE
-        id = " . $stats[$i]['id'];
-
-  echo $query . "\n";
-
-    try {
-      $ss = $db->Execute($query);
-    } catch (exception $err) {
-      $debug->log($jconf['log_dir'], $myjobid . ".log", "[ERROR] SQL query failed." . trim($query), $sendmail = false);
-      return false;
-    }
-
-  }
-
-echo "num: " . $i . "\n";
-  return true;
 }
 
 function removeStatsAll($stats_config, $isexec) {
@@ -745,46 +664,5 @@ function removeStatsAll($stats_config, $isexec) {
   return true;
 }
 
-function checkWowzaOpenRecords() {
- global $db, $debug, $myjobid, $app, $jconf;
-
-  // Select records open for 3 days
-  $startdate = date("Y-m-d H:i:s" , time() - 24 * 60 * 60 * 3);
-
-  $query = "
-    SELECT
-      id,
-      starttime
-      wowzalocalstreamname,
-      wowzaremotestreamname,
-      serverip,
-      clientip,
-      encoder
-    FROM
-      cdn_streaming_stats AS css
-    WHERE
-      css.endtime IS NULL AND
-      css.starttime < '" . $startdate . "'
-    ORDER BY starttime";
-
-  try {
-    $stats = $db->getArray($query);
-  } catch (exception $err) {
-    $debug->log($jconf['log_dir'], $myjobid . ".log", "[ERROR] SQL query failed." . trim($query), $sendmail = true);
-    return false;
-  }
-
-  // Log open records and send warning message
-  $msg_log = "";
-  for ( $i = 0; $i < count($stats); $i++ ) {
-    $msg_log .= print_r($stats[$i], true) . "\n";
-  }
-
-  if ( !empty($msg_log) ) {
-    $debug->log($jconf['log_dir'], $myjobid . ".log", "[WARN] Records with open endtime exist! Please check!\n\n" . $msg_log, $sendmail = true);
-  }
-
-  return true;
-}
 
 ?>
