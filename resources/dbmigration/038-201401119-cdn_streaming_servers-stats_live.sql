b9522db5682e1b2aaa3bf97f84888e432cd3ad70
VSQ Conv <andras.kovacs@teleconnect.hu>
2014-11-19 18:01:01 +0100

    [touch:1210] stats table streaming szerverek+

--- resources/dbmigration/038-201401119-cdn_streaming_servers-stats_live.sql
+++ resources/dbmigration/038-201401119-cdn_streaming_servers-stats_live.sql
@@ -1,3 +1,5 @@
 ALTER TABLE  `cdn_streaming_servers` ADD  `description` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER  `shortname`;
 ALTER TABLE  `cdn_streaming_servers` ADD  `location` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER  `country`;
 ALTER TABLE  `statistics_live_5min` ADD  `streamingserverid` INT(11) UNSIGNED NOT NULL AFTER  `livefeedstreamid`;
+ALTER TABLE  `statistics_live_hourly` ADD  `streamingserverid` INT(11) UNSIGNED NOT NULL AFTER  `livefeedstreamid`;
+ALTER TABLE  `statistics_live_daily` ADD  `streamingserverid` INT(11) UNSIGNED NOT NULL AFTER  `livefeedstreamid`;
