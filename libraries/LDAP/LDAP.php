2b2f9cea55b078af0f95e593b50e997c5869e7cc
Peter SZTANOJEV <sztanpet@gmail.com>
2015-01-17 11:22:51 +0100

    ldap: legyen fallback escape function

--- libraries/LDAP/LDAP.php
+++ libraries/LDAP/LDAP.php
@@ -1,6 +1,82 @@
 <?php
 namespace LDAP;
 
+// code from http://stackoverflow.com/questions/8560874/php-ldap-add-function-to-escape-ldap-special-characters-in-dn-syntax
+// by DaveRandom
+if (!function_exists('ldap_escape')) {
+    define('LDAP_ESCAPE_FILTER', 0x01);
+    define('LDAP_ESCAPE_DN',     0x02);
+
+    /**
+     * @param string $subject The subject string
+     * @param string $ignore Set of characters to leave untouched
+     * @param int $flags Any combination of LDAP_ESCAPE_* flags to indicate the
+     *                   set(s) of characters to escape.
+     * @return string
+     */
+    function ldap_escape($subject, $ignore = '', $flags = 0)
+    {
+        static $charMaps = array(
+            LDAP_ESCAPE_FILTER => array('\\', '*', '(', ')', "\x00"),
+            LDAP_ESCAPE_DN     => array('\\', ',', '=', '+', '<', '>', ';', '"', '#'),
+        );
+
+        // Pre-process the char maps on first call
+        if (!isset($charMaps[0])) {
+            $charMaps[0] = array();
+            for ($i = 0; $i < 256; $i++) {
+                $charMaps[0][chr($i)] = sprintf('\\%02x', $i);;
+            }
+
+            for ($i = 0, $l = count($charMaps[LDAP_ESCAPE_FILTER]); $i < $l; $i++) {
+                $chr = $charMaps[LDAP_ESCAPE_FILTER][$i];
+                unset($charMaps[LDAP_ESCAPE_FILTER][$i]);
+                $charMaps[LDAP_ESCAPE_FILTER][$chr] = $charMaps[0][$chr];
+            }
+
+            for ($i = 0, $l = count($charMaps[LDAP_ESCAPE_DN]); $i < $l; $i++) {
+                $chr = $charMaps[LDAP_ESCAPE_DN][$i];
+                unset($charMaps[LDAP_ESCAPE_DN][$i]);
+                $charMaps[LDAP_ESCAPE_DN][$chr] = $charMaps[0][$chr];
+            }
+        }
+
+        // Create the base char map to escape
+        $flags = (int)$flags;
+        $charMap = array();
+        if ($flags & LDAP_ESCAPE_FILTER) {
+            $charMap += $charMaps[LDAP_ESCAPE_FILTER];
+        }
+        if ($flags & LDAP_ESCAPE_DN) {
+            $charMap += $charMaps[LDAP_ESCAPE_DN];
+        }
+        if (!$charMap) {
+            $charMap = $charMaps[0];
+        }
+
+        // Remove any chars to ignore from the list
+        $ignore = (string)$ignore;
+        for ($i = 0, $l = strlen($ignore); $i < $l; $i++) {
+            unset($charMap[$ignore[$i]]);
+        }
+
+        // Do the main replacement
+        $result = strtr($subject, $charMap);
+
+        // Encode leading/trailing spaces if LDAP_ESCAPE_DN is passed
+        if ($flags & LDAP_ESCAPE_DN) {
+            if ($result[0] === ' ') {
+                $result = '\\20' . substr($result, 1);
+            }
+            if ($result[strlen($result) - 1] === ' ') {
+                $result = substr($result, 0, -1) . '\\20';
+            }
+        }
+
+        return $result;
+    }
+}
+
 class LDAP {
   protected $conn;
   protected $bootstrap;
@@ -64,9 +140,9 @@ class LDAP {
     return new \LDAP\Search( $this->conn, $result );
   }
 
-  public function escape( $value, $isdn = false ) {
+  public static function escape( $value, $isdn = false ) {
     $flags = $isdn? LDAP_ESCAPE_DN: LDAP_ESCAPE_FILTER;
-    return \ldap_escape( $value, null, $flags );
+    return ldap_escape( $value, null, $flags );
   }
 
   public static function getTimestamp( $ldaptimestamp ) {
