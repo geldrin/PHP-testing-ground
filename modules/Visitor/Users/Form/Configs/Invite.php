418a9a667c8c6e433598325979694a162887c9af
Peter SZTANOJEV <sztanpet@gmail.com>
2015-01-25 14:49:11 +0100

    [in progress: 1261] Meghívás: csoportba meghívás nem szerepel

--- modules/Visitor/Users/Form/Configs/Invite.php
+++ modules/Visitor/Users/Form/Configs/Invite.php
@@ -313,7 +313,10 @@ $config = $config + array(
       FROM groups AS g
       WHERE
         organizationid = '" . $this->controller->organization['id'] . "' AND
-        source        <> 'directory'
+        (
+          source <> 'directory' OR
+          source IS NULL
+        )
       ORDER BY g.name DESC
     ",
     'validation'  => array(
