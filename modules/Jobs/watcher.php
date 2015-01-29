3e4cc2a3a93bd0ff0488777d5427f6e70f021d28
VSQ Conv <andras.kovacs@teleconnect.hu>
2015-01-29 12:58:11 +0100

    [touch:1205] watcher kozmetika, logging

--- modules/Jobs/watcher.php
+++ modules/Jobs/watcher.php
@@ -101,30 +101,15 @@ if ( $jobs_isstopped and !empty($jobs_stopped) ) {
 
 //// Is any conversion running?
 $isrunning_mediaconv = false;
-$isrunning_contentconv = false;
 // Media conversion/ffmpeg: check if running
 $output = array();
 $check_string = $jconf['media_dir'];
 $cmd = 'ps uax | grep "ffmpeg.*' . $check_string . '" | grep -v "grep" | wc -l 2>&1';
 exec($cmd, $output, $result);
 if ( isset($output[0]) ) if ( $output[0] > 0 ) $isrunning_mediaconv = true;
-// Content conversion/ffmpeg
-$output = array();
-$check_string = $jconf['content_dir'];
-$cmd = 'ps uax | grep "ffmpeg.*' . $check_string . '" | grep -v "grep" | wc -l 2>&1';
-exec($cmd, $output, $result);
-if ( isset($output[0]) ) if ( $output[0] > 0 ) $isrunning_contentconv = true;
-
-// !!! NEW CONV: REMOVE !!!
-// Content conversion/VLC: check if running
-$output = array();
-$cmd = 'ps uax | grep "cvlc" | grep -v "grep" | wc -l 2>&1';
-exec($cmd, $output, $result);
-if ( isset($output[0]) ) if ( $output[0] > 0 ) $isrunning_contentconv = true;
-// !!! NEW CONV: REMOVE !!!
 
 // List and log all running jobs
-$grep_target = "php -f " . $jconf['job_dir'];
+$grep_target = "php.*" . $jconf['job_dir'];
 $processes = `ps uax | egrep "$grep_target(job_|watcher)" | grep -v "grep" 2>&1`;
 file_put_contents($jconf['temp_dir'] . 'processids', $processes);
 $debug->log($jconf['log_dir'], $jconf['jobid_watcher'] . ".log", "[INFO] All running jobs:\n" . $processes, $sendmail = false);
@@ -140,50 +125,44 @@ foreach ( $jobs[$node_role] as $job => $job_info ) {
 	$output = array();
 
 	// Check if this job is running
-	$debug->log($jconf['log_dir'], $jconf['jobid_watcher'] . ".log", "[INFO] Check if " . $job . ".php is running.", $sendmail = false);
-	$cmd = 'ps uax | grep "' . $grep_target . $job . '.php" | grep -v "grep" | wc -l 2>&1';
-	exec($cmd, $output, $result);
-	$debug->log($jconf['log_dir'], $jconf['jobid_watcher'] . ".log", "[INFO] Command executed: " . $cmd . "\nResult: " . $result . "\nOutput: " . var_export($output, 1), $sendmail = false);
-
-	$num_jobs = $output[0];
-
+    $num_jobs = isProcessExists($grep_target . $job . ".php");
+    $isrunningjob = "NOT RUNNING";
+    if ( $num_jobs > 0 ) $isrunningjob = "RUNNING (" . $num_jobs . ")";
+    $debug->log($jconf['log_dir'], $jconf['jobid_watcher'] . ".log", "[INFO] " . $job . ".php status: " . $isrunningjob, $sendmail = false);
+    
 	$body  = "";
 	$body .= "SITE: " . $app->config['baseuri'] . "\n";
 	$body .= "NODE: " . $app->config['node_sourceip'] . "\n";
 	$body .= "NODE UPTIME: " . $uptime . "\n";
 	$body .= "JOB: " . $job . ".php\n";
 	$body .= "WATCHER PROCESSID: " . PROCESSID . "\n";
-	$body .= "COMMAND: " . $cmd . "\n";
-	$body .= "COMMAND OUTPUT:\n" . var_export($output, 1) . "\n";
-	$body .= "RESULT: " . $result . "\n";
-	$body .= "PROCESSES:\n\n" . $processes . "\n";
 	$body .= "TIME: " . date("Y-m-d G:i:s.u");
 
 	switch ( $num_jobs ) {
 		case '0':
-			// Not running: execute script and send alert message
-			$debug->log($jconf['log_dir'], $jconf['jobid_watcher'] . ".log", "[WARN] Job " . $job . ".php is not running!", $sendmail = false);
 
 			// Problem: sometime ps does not give result while the process is running. Maybe some strange transient is occured? As a workaround, let's wait for some time and double check if process is running.
 			sleep(1);
-			$cmd = 'ps uax | grep "' . $grep_target . $job . '.php" | grep -v "grep" | wc -l 2>&1';
-			exec($cmd, $output, $result);
-			$debug->log($jconf['log_dir'], $jconf['jobid_watcher'] . ".log", "[INFO] Job " . $job . ".php recheck: " . $cmd . "\nResult: " . $result . "\nOutput: " . var_export($output, 1), $sendmail = false);
+            $num_jobs_recheck = isProcessExists($grep_target . $job . ".php");
+            $isrunningjob = "NOT RUNNING";
+            if ( $num_jobs_recheck > 0 ) {
+                $isrunningjob = "RUNNING (" . $num_jobs_recheck . ")";
+                $debug->log($jconf['log_dir'], $jconf['jobid_watcher'] . ".log", "[INFO] " . $job . ".php RECHECK status: " . $isrunningjob, $sendmail = false);
+                exit;
+                break;
+            }
 
-			if ( $output[0] > 0 ) {
-				$debug->log($jconf['log_dir'], $jconf['jobid_watcher'] . ".log", "[OK] Job " . $job . ".php recheck: process is running.", $sendmail = false);
-				break;
-			}
-
-			$inditas  = microtime(true);
+            $debug->log($jconf['log_dir'], $jconf['jobid_watcher'] . ".log", "[INFO] " . $job . ".php RECHECK status: " . $isrunningjob, $sendmail = false);
+            
+			$cmd_start  = microtime(true);
 			$command = "/usr/bin/php -f " . $jconf['job_dir'] . $job . ".php > /dev/null &";
 			exec($command, $output, $result);
-			$megallas = microtime( true );
+			$cmd_end = microtime(true);
 			$output_string = implode("\n", $output);
 
 			$msg  = "Did not find " . $job . ".php process.\n\nDetailed information:\n\n";
 			$msg .= $body . "\n\n";
-			$msg .= "Job restarted, information:\n";
+			$msg .= "Job reinit, information:\n";
 			$msg .= "COMMAND: " . $command . "\n";
 			$msg .= "OUTPUT: " . $output_string . "\n";
 			$msg .= "RESULT: ";
@@ -192,36 +171,30 @@ foreach ( $jobs[$node_role] as $job => $job_info ) {
 			} else {
 				$msg .= "OK\n";
 			}
-			$msg .= "COMMAND DURATION: " . ($megallas - $inditas) . "usec\n";
+			$msg .= "COMMAND DURATION: " . ($cmd_end - $cmd_start) . "usec\n";
 
 			// Log: log to file using no DB, then send mail
 			$debug->log($jconf['log_dir'], $jconf['jobid_watcher'] . ".log", $msg, $sendmail = false);
-			sendHTMLEmail_errorWrapper('[' . $app->bootstrap->config['siteid'] . ' log]: ' . substr( $msg, 0, 100 ), $msg, false);
+			sendHTMLEmail_errorWrapper('[' . $app->bootstrap->config['siteid'] . ' log]: ' . substr($msg, 0, 100), $msg, false);
 
 			break;
 		case '1':
 			// Running: check watchdog time difference (if larger and ffmpeg is not running...)
-
 			$time = @filemtime( $app->config['datapath'] . 'watchdog/' . $job . '.php.watchdog' );
 
 			$isrunning = false;
 			if ( stripos($job, "media") !== false ) $isrunning = $isrunning_mediaconv;
-			if ( stripos($job, "content") !== false ) $isrunning = $isrunning_contentconv;
 			if ( ( ( time() - $time ) >= $job_info['watchdogtimeoutsecs'] ) && ( $isrunning === false ) ) {
-				$msg  = "Job " . $job . ".php is stalled.\n\nDetailed information:\n\n";
+				$msg  = "Job " . $job . ".php is stalled?\n\nDetailed information:\n\n";
 				$msg .= $body . "\n\n";
 				// Log: log to file using no DB, then send mail
 				$debug->log($jconf['log_dir'], $jconf['jobid_watcher'] . ".log", $msg, $sendmail = false);
 				sendHTMLEmail_errorWrapper('[' . $app->bootstrap->config['siteid'] . ' log]: ' . substr( $msg, 0, 100 ), $msg, false);
-			} else {
-				$debug->log($jconf['log_dir'], $jconf['jobid_watcher'] . ".log", "[OK] Job " . $job . ".php is running. Watchdog time check is OK.", $sendmail = false);
 			}
 
 			break;
 		default:
 			// Running in more than 1 instances: undefined (send alert)
-			$debug->log($jconf['log_dir'], $jconf['jobid_watcher'] . ".log", "[ERROR] Job " . $job . ".php is running in more instances.", $sendmail = false);
-
 			$msg  = "Job " . $job . ".php is running in more instances.\n\nDetailed information:\n\n";
 			$msg .= $body . "\n\n";
 			// Log: log to file using no DB, then send mail
