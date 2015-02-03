e198c6e0212603c2395225c2da4b2fb2664e6273
VSQ Conv <andras.kovacs@teleconnect.hu>
2015-02-03 10:19:17 +0100

    [touch:1123] db mezok final

--- resources/dbmigration/050-20150102-converter-nodes.sql
+++ resources/dbmigration/050-20150102-converter-nodes.sql
@@ -1,9 +1,13 @@
 ALTER TABLE  `converter_nodes` ADD  `status` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER  `default`,
 ADD  `type` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL AFTER  `shortname`,
-ADD  `storagesystemtotal` BIGINT( 20 ) UNSIGNED NULL AFTER  `status`,
-ADD  `storagesystemfree` BIGINT( 20 ) UNSIGNED NULL AFTER  `storagesystemtotal`,
-ADD  `storagetemptotal` BIGINT( 20 ) UNSIGNED NULL AFTER  `storagesystemfree`,
-ADD  `storagetempfree` BIGINT( 20 ) UNSIGNED NULL AFTER  `storagetemptotal`,
-ADD  `cpuusage` FLOAT UNSIGNED NULL DEFAULT NULL AFTER  `storagetempfree`;
+ADD  `storagesystemtotal` BIGINT( 20 ) UNSIGNED NULL DEFAULT NULL AFTER  `status`,
+ADD  `storagesystemfree` BIGINT( 20 ) UNSIGNED NULL DEFAULT NULL AFTER  `storagesystemtotal`,
+ADD  `storagetotal` BIGINT( 20 ) UNSIGNED NULL DEFAULT NULL AFTER  `storagesystemfree`,
+ADD  `storageworktotal` BIGINT( 20 ) UNSIGNED NULL DEFAULT NULL AFTER  `storagesystemfree`,
+ADD  `storageworkfree` BIGINT( 20 ) UNSIGNED NULL DEFAULT NULL AFTER  `storageworktotal`,
+ADD  `storagefree` BIGINT( 20 ) UNSIGNED NULL DEFAULT NULL AFTER  `storagetotal`,
+ADD  `cpuusage` FLOAT UNSIGNED NULL DEFAULT NULL AFTER  `storagefree`;
 
 RENAME TABLE  `converter_nodes` TO  `infrastructure_nodes`;
+
+UPDATE infrastructure_nodes SET TYPE =  'converter' WHERE id >= 1;
