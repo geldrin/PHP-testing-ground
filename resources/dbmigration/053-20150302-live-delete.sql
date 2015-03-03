996434131e16bb9a41651ecdc96addfe77a4da95
Peter SZTANOJEV <sztanpet@gmail.com>
2015-03-03 18:27:03 +0100

    db migracios script fix

--- resources/dbmigration/053-20150302-live-delete.sql
+++ resources/dbmigration/053-20150302-live-delete.sql
@@ -1,13 +1,1 @@
-ALTER TABLE  `livefeed_streams` ADD  `quality_temp` TEXT NULL ,
-ADD  `weight` INT NOT NULL DEFAULT  '100';
-
-UPDATE `livefeed_streams`
-SET quality_temp = 'Normál'
-WHERE quality = 0;
-
-UPDATE `livefeed_streams`
-SET quality_temp = 'HD'
-WHERE quality = 1;
-
-ALTER TABLE  `livefeed_streams` DROP  `quality`;
-ALTER TABLE  `livefeed_streams` CHANGE  `quality_temp`  `quality` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
+ALTER TABLE  `livefeeds` ADD  `status` TEXT NULL;
