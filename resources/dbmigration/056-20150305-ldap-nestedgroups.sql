2ee5047a05090877428c8867217ca22141c1c0a3
Telenor <andras.kovacs@videosqr.com>
2015-03-09 18:13:34 +0100

    [touch:1330] ldap nested DB

---
+++ resources/dbmigration/056-20150305-ldap-nestedgroups.sql
@@ -0,0 +1,7 @@
+ALTER TABLE  `groups` ADD  `organizationdirectoryuserslastsynchronized` DATETIME NULL DEFAULT NULL AFTER  `organizationdirectoryldapwhenchanged`,
+    ADD  `ispermanent` INT( 10 ) NOT NULL DEFAULT  '0' AFTER  `organizationdirectoryuserslastsynchronized`;
+    
+ALTER TABLE  `groups_members` ADD  `userexternalid` VARCHAR( 255 ) NULL DEFAULT NULL AFTER  `userid`;
+
+ALTER TABLE  `organizations_directories` ADD  `ldapgroupaccessid` INT( 10 ) UNSIGNED NULL DEFAULT NULL AFTER  `ldapusertreedn`;
+ALTER TABLE  `organizations_directories` ADD  `ldapgroupadminid` INT( 10 ) UNSIGNED NULL DEFAULT NULL AFTER  `ldapgroupaccess`;
