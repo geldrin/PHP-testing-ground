d0739e832be2540d61773bade0cb9f618ec6a7ef
Gergely Lukacsy <gergely.lukacsy@streamnet.hu>
2015-04-09 17:18:15 +0200

    [touch: 1380] promo aggreg. REGEXP modositas

--- modules/Jobs/job_accounting.php
+++ modules/Jobs/job_accounting.php
@@ -114,7 +114,7 @@ for ($i = 0; $i < count($org_contracts); $i++ ) {
 			u.isusergenerated = 1 AND
 			u.firstloggedin > '" . $firstloggedin_startdate . "' AND
 			u.firstloggedin < '" . $firstloggedin_enddate . "' AND
-            u.email REGEXP '^felh[0-9][0-9][0-9][0-9]@.+'
+			u.email REGEXP '^felh[\d]+@.+'
 		GROUP BY
 			u.id
 		ORDER BY
