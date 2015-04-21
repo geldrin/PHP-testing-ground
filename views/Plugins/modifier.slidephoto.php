1a652e5b7c3fb138ba15fb80d8da35f465973be3
Peter SZTANOJEV <sztanpet@gmail.com>
2015-04-21 16:06:12 +0200

    [in progress: 1335] Keresés OCR adatokban, kep linkeles

--- views/Plugins/modifier.slidephoto.php
+++ views/Plugins/modifier.slidephoto.php
@@ -1,11 +1,26 @@
 <?php
 
-function smarty_modifier_slidephoto( $data, $staticuri ) {
+function smarty_modifier_slidephoto( $data, $staticuri, $type = null ) {
+  switch( $type ) {
+    case 'wide':
+      $replace = $bootstrap->config['videothumbnailresolutions']['wide'];
+      break;
+    case 'player':
+      $replace = $bootstrap->config['videothumbnailresolutions']['player'];
+      break;
+    default:
+      $replace = $bootstrap->config['videothumbnailresolutions']['4:3'];
+      break;
+  }
+  $pos = strpos( $replace, 'x');
+  $dir = substr( $replace, 0, $pos ); // az x elotti resz erdekel minket
 
-  return
-    $staticuri . 'files/recordings/' .
-    \Springboard\Filesystem::getTreeDir( $data['recordingid'] ) . '/slides/' .
+  // https://dam.codebasehq.com/projects/teleconnect/tickets/1335
+  return sprintf("%sfiles/recordings/%s/ocr/%s/%s_%s.jpg",
+    $staticuri,
+    \Springboard\Filesystem::getTreeDir( $data['recordingid'] ),
+    $dir,
+    $data['recordingid'],
     $data['id']
-  ;
-
+  );
 }
