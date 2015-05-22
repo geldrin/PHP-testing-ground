4208156d29fdc59492fb3d111496c25724099a40
VSQ Conv <andras.kovacs@teleconnect.hu>
2015-05-22 17:02:17 +0200

    [touch:1143] wowza monitor xml parsing hibakezeles

--- resources/server_config/frontend/wowza/wowza_monitor/wowza_monitor.php
+++ resources/server_config/frontend/wowza/wowza_monitor/wowza_monitor.php
@@ -1,160 +1,30 @@
 <?php
 
-/*
-
-<WowzaMediaServer>
-	<ConnectionsCurrent>6</ConnectionsCurrent>
-	<ConnectionsTotal>293</ConnectionsTotal>
-	<ConnectionsTotalAccepted>287</ConnectionsTotalAccepted>
-	<ConnectionsTotalRejected>6</ConnectionsTotalRejected>
-	<MessagesInBytesRate>403858.0</MessagesInBytesRate>
-	<MessagesOutBytesRate>175126.0</MessagesOutBytesRate>
-	<VHost>
-		<Name>_defaultVHost_</Name>
-		<TimeRunning>505435.573</TimeRunning>
-		<ConnectionsLimit>0</ConnectionsLimit>
-		<ConnectionsCurrent>6</ConnectionsCurrent>
-		<ConnectionsTotal>293</ConnectionsTotal>
-		<ConnectionsTotalAccepted>287</ConnectionsTotalAccepted>
-		<ConnectionsTotalRejected>6</ConnectionsTotalRejected>
-		<MessagesInBytesRate>403858.0</MessagesInBytesRate>
-		<MessagesOutBytesRate>175126.0</MessagesOutBytesRate>
-		<Application>
-			<Name>devvsqlive</Name>
-			<Status>loaded</Status>
-			<TimeRunning>505434.616</TimeRunning>
-			<ConnectionsCurrent>0</ConnectionsCurrent>
-			<ConnectionsTotal>0</ConnectionsTotal>
-			<ConnectionsTotalAccepted>0</ConnectionsTotalAccepted>
-			<ConnectionsTotalRejected>0</ConnectionsTotalRejected>
-			<MessagesInBytesRate>403855.0</MessagesInBytesRate>
-			<MessagesOutBytesRate>0.0</MessagesOutBytesRate>
-			<ApplicationInstance>
-				<Name>_definst_</Name>
-				<TimeRunning>505434.575</TimeRunning>
-				<ConnectionsCurrent>0</ConnectionsCurrent>
-				<ConnectionsTotal>0</ConnectionsTotal>
-				<ConnectionsTotalAccepted>0</ConnectionsTotalAccepted>
-				<ConnectionsTotalRejected>0</ConnectionsTotalRejected>
-				<MessagesInBytesRate>403855.0</MessagesInBytesRate>
-				<MessagesOutBytesRate>0.0</MessagesOutBytesRate>
-				<Stream>
-					<Name>461272.stream</Name>
-					<SessionsFlash>0</SessionsFlash>
-					<SessionsCupertino>0</SessionsCupertino>
-					<SessionsSanJose>0</SessionsSanJose>
-					<SessionsSmooth>0</SessionsSmooth>
-					<SessionsRTSP>0</SessionsRTSP>
-					<SessionsTotal>0</SessionsTotal>
-				</Stream>
-				<Stream>
-					<Name>549088.stream</Name>
-					<SessionsFlash>0</SessionsFlash>
-					<SessionsCupertino>0</SessionsCupertino>
-					<SessionsSanJose>0</SessionsSanJose>
-					<SessionsSmooth>0</SessionsSmooth>
-					<SessionsRTSP>0</SessionsRTSP>
-					<SessionsTotal>0</SessionsTotal>
-				</Stream>
-			</ApplicationInstance>
-		</Application>
-		<Application>
-			<Name>vsq</Name>
-			<Status>loaded</Status>
-			<TimeRunning>10918.991</TimeRunning>
-			<ConnectionsCurrent>6</ConnectionsCurrent>
-			<ConnectionsTotal>21</ConnectionsTotal>
-			<ConnectionsTotalAccepted>21</ConnectionsTotalAccepted>
-			<ConnectionsTotalRejected>0</ConnectionsTotalRejected>
-			<MessagesInBytesRate>3.0</MessagesInBytesRate>
-			<MessagesOutBytesRate>175126.0</MessagesOutBytesRate>
-			<ApplicationInstance>
-				<Name>_definst_</Name>
-				<TimeRunning>10918.976</TimeRunning>
-				<ConnectionsCurrent>6</ConnectionsCurrent>
-				<ConnectionsTotal>21</ConnectionsTotal>
-				<ConnectionsTotalAccepted>21</ConnectionsTotalAccepted>
-				<ConnectionsTotalRejected>0</ConnectionsTotalRejected>
-				<MessagesInBytesRate>3.0</MessagesInBytesRate>
-				<MessagesOutBytesRate>175126.0</MessagesOutBytesRate>
-				<Stream>
-					<Name>294%2F294%2F294_content_lq.mp4</Name>
-					<SessionsFlash>1</SessionsFlash>
-					<SessionsCupertino>0</SessionsCupertino>
-					<SessionsSanJose>0</SessionsSanJose>
-					<SessionsSmooth>0</SessionsSmooth>
-					<SessionsRTSP>0</SessionsRTSP>
-					<SessionsTotal>1</SessionsTotal>
-				</Stream>
-				<Stream>
-					<Name>302%2F302%2F302_content_lq.mp4</Name>
-					<SessionsFlash>1</SessionsFlash>
-					<SessionsCupertino>0</SessionsCupertino>
-					<SessionsSanJose>0</SessionsSanJose>
-					<SessionsSmooth>0</SessionsSmooth>
-					<SessionsRTSP>0</SessionsRTSP>
-					<SessionsTotal>1</SessionsTotal>
-				</Stream>
-				<Stream>
-					<Name>294%2F294%2F294_video_lq.mp4</Name>
-					<SessionsFlash>1</SessionsFlash>
-					<SessionsCupertino>0</SessionsCupertino>
-					<SessionsSanJose>0</SessionsSanJose>
-					<SessionsSmooth>0</SessionsSmooth>
-					<SessionsRTSP>0</SessionsRTSP>
-					<SessionsTotal>1</SessionsTotal>
-				</Stream>
-				<Stream>
-					<Name>302%2F302%2F302_video_lq.mp4</Name>
-					<SessionsFlash>1</SessionsFlash>
-					<SessionsCupertino>0</SessionsCupertino>
-					<SessionsSanJose>0</SessionsSanJose>
-					<SessionsSmooth>0</SessionsSmooth>
-					<SessionsRTSP>0</SessionsRTSP>
-					<SessionsTotal>1</SessionsTotal>
-				</Stream>
-				<Stream>
-					<Name>295%2F295%2F295_video_lq.mp4</Name>
-					<SessionsFlash>1</SessionsFlash>
-					<SessionsCupertino>0</SessionsCupertino>
-					<SessionsSanJose>0</SessionsSanJose>
-					<SessionsSmooth>0</SessionsSmooth>
-					<SessionsRTSP>0</SessionsRTSP>
-					<SessionsTotal>1</SessionsTotal>
-				</Stream>
-				<Stream>
-					<Name>295%2F295%2F295_content_lq.mp4</Name>
-					<SessionsFlash>1</SessionsFlash>
-					<SessionsCupertino>0</SessionsCupertino>
-					<SessionsSanJose>0</SessionsSanJose>
-					<SessionsSmooth>0</SessionsSmooth>
-					<SessionsRTSP>0</SessionsRTSP>
-					<SessionsTotal>1</SessionsTotal>
-				</Stream>
-			</ApplicationInstance>
-		</Application>
-	</VHost>
-</WowzaMediaServer>
-
-*/
-
 define('BASE_PATH',	realpath( __DIR__ . '/../../../../..' ) . '/' );
 define('DEBUG', false );
 
 include_once( BASE_PATH . 'libraries/Springboard/Application/Cli.php');
 
+// Init
+$app = new Springboard\Application\Cli(BASE_PATH, false);
+
+// Load jobs configuration file
+$app->loadConfig('modules/Jobs/config_jobs.php');
+$jconf = $app->config['config_jobs'];
+$myjobid = $jconf['jobid_wowza_monitor'];
+
+// Log init
+$debug = Springboard\Debug::getInstance();
+
 set_time_limit(0);
 
-$islive = TRUE;
-
-// Init
-$app = new Springboard\Application\Cli(BASE_PATH, FALSE);
+$islive = true;
 
 // Establish database connection
 try {
 	$db = $app->bootstrap->getAdoDB();
 } catch (exception $err) {
-	echo "ERROR: cannot connect to DB.\n" . $err . "\n";
+    $debug->log($jconf['log_dir'], $myjobid . ".log", "[ERROR] Cannot connect to DB.\n" . $err, $sendmail = false);
 	exit -1;
 }
 
@@ -173,6 +43,7 @@ $query = "
 		cdn_streaming_servers AS ss
 	WHERE
 		ss.disabled = 0 AND
+        ss.type = 'wowza' AND
         adminuser IS NOT NULL AND
         monitoringpassword IS NOT NULL
 ";
@@ -180,7 +51,7 @@ $query = "
 try {
 	$monitor_servers = $db->getArray($query);
 } catch ( \Exception $e ) {
-	echo "[ERROR]: Cannot query streaming servers.\n";
+    $debug->log($jconf['log_dir'], $myjobid . ".log", "[ERROR] Cannot query streaming servers.\n" . $err, $sendmail = false);
 	exit -1;
 }
 
@@ -195,9 +66,9 @@ $wowza_app = "vsq";
 if ( isset($app->config['production']) && $app->config['production'] === false ) $wowza_app = "dev" . $wowza_app;
 
 // Wowza app: live or on demand config
-$islive = FALSE;
+$islive = false;
 if ( ( ( count($argv) > 1 ) && ( $argv[1] == 'live' ) ) or ( (count($argv) > 2) && ($argv[2] == 'live') ) ) {
-	$islive = TRUE;
+	$islive = true;
 	$wowza_app .= "live";
 }
 
@@ -216,6 +87,8 @@ apptotal.label Total " . $wowza_app . "
 $total_currentconnections = 0;
 $total_currentconnections_perapp = 0;
 
+var_dump($monitor_servers);
+
 for ($i = 0; $i < count($monitor_servers); $i++ ) {
 
 	$curl = curl_init();
@@ -228,31 +101,48 @@ for ($i = 0; $i < count($monitor_servers); $i++ ) {
 	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); 
 	curl_setopt($curl, CURLOPT_USERPWD, $monitor_servers[$i]['adminuser'] . ":" . $monitor_servers[$i]['password']);
 	curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
+echo $i . "\n";
 
 	$data = curl_exec($curl); 
 	if( curl_errno($curl) ){ 
-//		echo "CURL ERROR: " . curl_error($curl) . " " . $monitor_servers[$i]['server'] . "\n";;
+		$err = curl_error($curl);
 		curl_close($curl);
 		$monitor_servers[$i]['currentconnections'] = "0";		// Munin: undefined value
 		streamingServerUpdateDB($monitor_servers[$i]['id'], "unreachable", 0);
+        $debug->log($jconf['log_dir'], $myjobid . ".log", "[ERROR] Server " . $monitor_servers[$i]['server'] . " is unreachable.\n" . $err, $sendmail = true);
 		continue;
 	}
 
 	// Check if authentication failed
 	$header = curl_getinfo($curl);
 	if ( $header['http_code'] == 401 ) {
-//		echo "ERROR: HTTP 401. Cannot authenticate at " . $monitor_servers[$i]['server'] . "\n";
 		curl_close($curl); 
 		$monitor_servers[$i]['currentconnections'] = "0";		// Munin: undefined value
 		streamingServerUpdateDB($monitor_servers[$i]['id'], "autherror", 0);
+        $debug->log($jconf['log_dir'], $myjobid . ".log", "[ERROR] HTTP 401. Cannot authenticate to server " . $monitor_servers[$i]['server'] . ".\n" . $err, $sendmail = true);
 		continue;
 	}
 
 	// Process XML output
 
 	// Open XML data
+    libxml_use_internal_errors(true);
 	$wowza_xml = simplexml_load_string($data);
 
+    // Valid XML?
+    $xml = explode("\n", $data);
+    if ( $wowza_xml === false ) {
+        
+        $err = "";
+        foreach(libxml_get_errors() as $error) {
+            $err .= $error->message . "\t";
+        }
+        libxml_clear_errors();
+        
+        $debug->log($jconf['log_dir'], $myjobid . ".log", "[ERROR] Cannot parse XML returned by Wowza. Error:\n\n" . $err, $sendmail = false);
+        continue;
+    }
+    
 	// Total number of clients connected to server
 	$currentconnections = 0 + (string)$wowza_xml->ConnectionsCurrent;
 
@@ -277,13 +167,6 @@ for ($i = 0; $i < count($monitor_servers); $i++ ) {
 	curl_close($curl); 
 }
 
-// Server total load
-/*if ( $total_currentconnections >= 0 ) {
-	echo "total.value " . $total_currentconnections . "\n";
-} else {
-	echo "total.value 0\n";
-} */
-
 // Per app total load
 if ( $total_currentconnections_perapp >= 0 ) {
 	echo "apptotal.value " . $total_currentconnections_perapp . "\n";
@@ -291,10 +174,7 @@ if ( $total_currentconnections_perapp >= 0 ) {
 	echo "apptotal.value 0\n";
 }
 
-//echo $wowza_app . "\n";
-//var_dump($monitor_servers);
 foreach( $monitor_servers as $server ) {
-//var_dump($server);
 	if ( isset($server[$wowza_app . '_currentconnections']) ) {
 		if ( $server[$wowza_app . '_currentconnections'] >= 0 ) {
 			echo $server['shortname'] . ".value " . $server[$wowza_app . '_currentconnections'] . "\n";
@@ -307,7 +187,7 @@ foreach( $monitor_servers as $server ) {
 exit;
 
 function streamingServerUpdateDB($id, $reachable, $currentload) {
- global $db;
+ global $db, $debug, $jconf, $myjobid;
 
 	// Query streaming servers and passwords
 	$query = "
@@ -323,10 +203,11 @@ function streamingServerUpdateDB($id, $reachable, $currentload) {
 		$rs = $db->Execute($query);
 	} catch ( \Exception $e ) {
 		echo "[ERROR]: Cannot update streaming server record.\n" .  $err . "\n";
-		return FALSE;
+        $debug->log($jconf['log_dir'], $myjobid . ".log", "[ERROR] Cannot update streaming server record.\n" .  $err, $sendmail = false);
+		return false;
 	}
 
-	return TRUE;
+	return true;
 }
 
 ?>
