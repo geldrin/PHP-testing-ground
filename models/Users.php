fd202486873c9de35d2dfd73565e29b52aa42cc5
Peter SZTANOJEV <sztanpet@gmail.com>
2015-06-23 13:03:10 +0200

    login cookie check fix ha modosult a password vagy kitiltodott a user

--- models/Users.php
+++ models/Users.php
@@ -1010,7 +1010,7 @@ class Users extends \Springboard\Model {
 
     // a hash valtozzon ha a user passwordot valt, elfelejtette a passwordjet,
     // vagy kitiltjak
-    if ( md5( $row['password'] . $row['validationcode'] . $row['disabled'] ) != $values[2] )
+    if ( empty( $row ) or md5( $row['password'] . $row['validationcode'] . $row['disabled'] ) != $values[2] )
       return array();
 
     return $row;
