39afe7f0ca0a6564fc30065587302ba857c5749e
Peter SZTANOJEV <sztanpet@gmail.com>
2016-01-27 18:00:24 +0100

    migracios script fix

--- resources/dbmigration/087-20160127-organizations_streamingprotocols.sql
+++ resources/dbmigration/087-20160127-organizations_streamingprotocols.sql
@@ -3,4 +3,4 @@ ALTER TABLE `organizations`
 ADD `ondemandhdsenabled` int not null default '0',
 ADD `livehdsenabled` int not null default '0' AFTER `ondemandhdsenabled`,
 ADD `ondemandhlsenabledandroid` int not null default '0' AFTER `livehdsenabled`,
-ADD `livehlsenabledandroid` int not null default '0' AFTER `ondemandhdsenabledandroid`;
+ADD `livehlsenabledandroid` int not null default '0' AFTER `ondemandhlsenabledandroid`;
