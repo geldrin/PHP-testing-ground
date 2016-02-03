0984ef491b542082b1ab0a3e7dbe61e447856ee0
Peter SZTANOJEV <sztanpet@gmail.com>
2016-02-03 19:12:13 +0100

    migracio tweak

--- resources/dbmigration/092-20160202-org_mainpage2.sql
+++ resources/dbmigration/092-20160202-org_mainpage2.sql
@@ -4,15 +4,15 @@ ALTER TABLE `recordings`
 ADD `combinedratingpermonth` int(11) NOT NULL DEFAULT '0';
 
 UPDATE recordings
-SET combinedratingperweek = (
+SET combinedratingperweek = IFNULL((
   ratingthisweek *
   ( 100 * numberofratingsthisweek / numberofviewsthisweek ) *
   numberofviewsthisweek
-);
+), 0);
 
 UPDATE recordings
-SET combinedratingpermonth = (
+SET combinedratingpermonth = IFNULL((
   ratingthismonth *
   ( 100 * numberofratingsthismonth / numberofviewsthismonth ) *
   numberofviewsthismonth
-);
+), 0);
