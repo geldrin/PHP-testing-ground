07fcc424c4443c787ff778d8b148c7b46354e25a
VSQ Conv <andras.kovacs@teleconnect.hu>
2016-02-23 19:05:22 +0100

    [touch:1742] job classok skeleton to be confirmed

---
+++ libraries/Videosquare/Model/Recordings.php
@@ -0,0 +1,84 @@
+<?php
+namespace Videosquare\Model;
+
+class Recordings extends \Springboard\Model {
+
+    private $recordingid;
+    private $recordingversionid;
+    
+    // Select recording
+    public function selectRecording($recordingid) {
+    
+        if ( empty($recordingid) ) return false;
+        $this->recordingid = $recordingid;
+        
+    }
+
+    // Select recording version
+    public function selectRecordingVersion($recordingversionid) {
+    
+        if ( empty($recordingversionid) ) return false;
+        
+        $this->recordingversionid = $recordingversionid;
+        
+        return true;
+    }
+
+    // Get recording status
+    public function getRecordingStatus($type = "recording") {
+
+        if ( ( $type != "recording" ) and ( $type != "content" ) ) return false;
+
+        $idx = "";
+        if ( $type == "content" ) $idx = "content";
+
+        $recordingObj = $this->bootstrap->getModel('recordings');
+        $recordingObj->select($this->recordingid);
+        $recording = $recordingObj->getRow();
+
+        return $recording[$idx . 'status'];
+    }
+
+    // Update recording status
+    public function updateRecordingStatus($status, $type = "recording") {
+        
+        $allowed_types = array('recording', 'content', 'mobile', 'ocr', 'smil', 'contentsmil');
+
+        if ( !in_array($type, $allowed_types, $strict = true) ) return false;
+        if ( empty($status) ) return false;
+
+        $idx = null;
+        if ( $type === 'recording' ) $idx = '';
+        else $idx = $type;
+        
+        if ( !empty($status) ) {
+            $values = array(
+                $idx . 'status' => $status
+            );
+        } else {
+            $values = array(
+                $idx . 'status' => null
+            );
+        }
+
+        $recordingVersionObj = $this->bootstrap->getModel('recordings');
+        $recordingVersionObj->select($this->recordingid);
+        $recordingVersionObj->updateRow($values);
+
+        // Update index photos
+        if ( ( $status == $this->bootstrap->config['config_jobs']['dbstatus_copystorage_ok'] ) and ( $type == "recording" ) ) {
+            $recordingObj = $this->bootstrap->getModel('recordings');
+            $recordingObj->select($this->recordingid);
+            $recordingObj->updateChannelIndexPhotos();
+        }
+
+        // Log status change
+        $this->debugLog("[INFO] Recording id = " . $this->recordingid . " " . $type . " status has been changed to '" . $status . "'.", false);
+
+        return true;
+    }
+
+    
+}
+
+?>
--- libraries/Videosquare/job_skeleton.php
+++ libraries/Videosquare/job_skeleton.php
@@ -1,13 +1,13 @@
 <?php
-namespace Videosquare;
+namespace Videosquare\Job;
 
 define('BASE_PATH',	realpath( __DIR__ . '/../..' ) . '/' );
 define('PRODUCTION', false );
 define('DEBUG', false );
 
-include_once('job.class.php');
+include_once('./Job/job.class.php');
 
-class myJob extends VSQJob {
+class myJob extends \Videosquare\Job\VSQJob {
 
     protected $needsLoop        = true;
 
@@ -20,8 +20,7 @@ class myJob extends VSQJob {
         echo "myJob is processing...\n";
         
         try {
-            $model = $this->bootstrap->getModel("\\Videosquare\\Job_Recordings");
-            //$model = $this->bootstrap->getModel("Job_Recordings");
+            $model = $this->bootstrap->getVSQModel("Recordings");
             //var_dump($model);
             $model->selectRecording(12);
             echo $model->getRecordingStatus() . "\n";
@@ -41,45 +40,13 @@ class myJob extends VSQJob {
 // Remove watchdog
 unlink("/home/conv/dev.videosquare.eu/data/watchdog/job_skeleton.php.watchdog");
 
-//$err = class1("\\Videosquare\Job_Recordings");
-//var_dump($err);
-
-$job = new \Videosquare\myJob(BASE_PATH, PRODUCTION);
+$job = new myJob(BASE_PATH, PRODUCTION);
 
 try {
   $job->run();
 } catch( Exception $err ) {
   $job->log( 'run() exception: ' . $err->getMessage(), false );
   throw $err;
-}
-
-function class1($class) {
-
-    if ( strpos( $class, '\\' ) === 0 )
-      $class = substr( $class, 1 );
-
-    $file  = str_replace('\\', '/', $class ) . '.php';
-
-    // not supported
-    if ( strpos( $class, '\\' ) === false )
-      return false;
-    elseif (
-             strpos( $class, 'Visitor\\' ) === 0 or
-             strpos( $class, 'Admin\\' ) === 0
-           )
-      return "modules/" . $file;
-    elseif ( strpos( $class, 'Model\\' ) === 0 ) {
-
-      $file =
-        "Model/" .
-        str_replace('Model/', '', $file )
-      ;
-
-      return $file;
-
-    } else
-      return "libraries/" . $file; 
- 
 }
 
 ?>
