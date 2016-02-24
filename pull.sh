a3f1b61814cbc501d5189213f14d39e4f2eb4119
Peter SZTANOJEV <sztanpet@gmail.com>
2016-02-24 15:28:19 +0100

    [in progress: 1747] abszolut symlinkeket hogy mukodjon is

--- pull.sh
+++ pull.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
+DIR=`dirname "$(readlink -f "$0")"`
+cd $DIR;
+
 if [ ! -x ".git/hooks/pre-commit" ] || [ ! -x ".git/hooks/post-merge"  ]; then
   rm -f ".git/hooks/pre-commit";
   rm -f ".git/hooks/post-merge";
-  ln -s "resources/git-hooks/pre-commit" ".git/hooks/pre-commit" || echo "Could not create pre-commit symlink";
-  ln -s "resources/git-hooks/post-merge" ".git/hooks/post-merge" || echo "Could not create post-merge symlink";
+  ln -s -f "$DIR/resources/git-hooks/pre-commit" ".git/hooks/pre-commit" || echo "Could not create pre-commit symlink";
+  ln -s -f "$DIR/resources/git-hooks/post-merge" ".git/hooks/post-merge" || echo "Could not create post-merge symlink";
 fi
 
 git pull origin
