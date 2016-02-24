0a98717a02578d47c8e8f5a2074352740675338c
Peter SZTANOJEV <sztanpet@gmail.com>
2016-02-24 14:40:13 +0100

    [in progress: 1747] egy git post-merge hook ami torli az organization cachet automatan

--- pull.sh
+++ pull.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
+if [ ! -x ".git/hooks/pre-commit" ] || [ ! -x ".git/hooks/post-merge"  ]; then
+  rm -f ".git/hooks/pre-commit";
+  rm -f ".git/hooks/post-merge";
+  ln -s "resources/git-hooks/pre-commit" ".git/hooks/pre-commit" || echo "Could not create pre-commit symlink";
+  ln -s "resources/git-hooks/post-merge" ".git/hooks/post-merge" || echo "Could not create post-merge symlink";
+fi
+
 git pull origin
 git submodule update
