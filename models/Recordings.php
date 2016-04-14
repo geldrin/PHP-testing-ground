3577eb4c36691ddb5e8f4acfbcdbb0c5a6c3fa7d
Peter SZTANOJEV <sztanpet@gmail.com>
2016-04-14 16:33:18 +0200

    [in progress: 1820] url fix5

--- models/Recordings.php
+++ models/Recordings.php
@@ -4130,7 +4130,7 @@ class Recordings extends \Springboard\Model {
     if ( $canDownload ) {
       // 111_video.mp4
       // 111_audio.mp3
-      $urlTemplate = "master/<id>_<type>";
+      $urlTemplate = "master/<id>_<type>.<ext>";
       $urlData = array(
         '<id>'       => $this->id,
         '<type>'     => 'video',
