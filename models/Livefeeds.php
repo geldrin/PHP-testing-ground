f385b7263bb7ba301f97e726c2497ad09f406880
Peter SZTANOJEV <sztanpet@gmail.com>
2016-04-26 16:25:18 +0200

    typo

--- models/Livefeeds.php
+++ models/Livefeeds.php
@@ -1464,7 +1464,7 @@ class Livefeeds extends \Springboard\Model {
 
       $liveRecModel->updateRow( $row );
       $this->db->query("
-        UDPATE livefeeds
+        UPDATE livefeeds
         SET livefeedrecordingid = NULL
         WHERE id = '" . $this->id . "'
         LIMIT 1
