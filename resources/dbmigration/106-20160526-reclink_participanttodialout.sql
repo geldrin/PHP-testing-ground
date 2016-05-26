df4148befbe442cf6037190b95aba9069cd7e6f4
VSQ Conv <andras.kovacs@teleconnect.hu>
2016-05-26 14:44:32 +0200

    [touch:1866] reclink predefined participant db fuck+fuck

--- resources/dbmigration/106-20160526-reclink_participanttodialout.sql
+++ resources/dbmigration/106-20160526-reclink_participanttodialout.sql
@@ -1,4 +1,6 @@
-ALTER TABLE  `recording_links` ADD  `participanttodialout` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER  `conferenceid`;
+ALTER TABLE  `recording_links` ADD  `autodialparticipant` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER  `conferenceid`;
 
-ALTER TABLE  `recording_links` ADD  `participanttodialoutprotocol` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER  `participanttodialout` ,
-ADD  `participanttodialoutdisplayname` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER  `participanttodialoutprotocol`;
+ALTER TABLE  `recording_links` ADD  `autodialparticipantprotocol` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER  `autodialparticipant` ,
+ADD  `autodialparticipantdisplayname` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER  `autodialparticipantprotocol`;
+
+ALTER TABLE  `livefeeds` ADD  `autodialparticipantid` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER  `vcrparticipantid`;
