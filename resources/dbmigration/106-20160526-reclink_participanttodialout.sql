346151aab6717dd6c4679e725f557c8dbed9e5b3
VSQ Conv <andras.kovacs@teleconnect.hu>
2016-05-26 14:40:26 +0200

    [touch:1866] reclink predefined participant db fuck

--- resources/dbmigration/106-20160526-reclink_participanttodialout.sql
+++ resources/dbmigration/106-20160526-reclink_participanttodialout.sql
@@ -1,5 +1,4 @@
-ALTER TABLE  `recording_links` ADD  `participanttodialout` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER  `conferenceid` ,
-ADD  `participanttodialoutid` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER  `participanttodialout`;
+ALTER TABLE  `recording_links` ADD  `participanttodialout` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER  `conferenceid`;
 
-ALTER TABLE  `recording_links` ADD  `participanttodialoutprotocol` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER  `participanttodialoutid` ,
+ALTER TABLE  `recording_links` ADD  `participanttodialoutprotocol` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER  `participanttodialout` ,
 ADD  `participanttodialoutdisplayname` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER  `participanttodialoutprotocol`;
