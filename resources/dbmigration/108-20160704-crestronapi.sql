4d61b30a57f6be40c09995dab40193760a9316b4
Peter SZTANOJEV <sztanpet@gmail.com>
2016-07-12 18:36:23 +0200

    crestron db migracios script fix

--- resources/dbmigration/108-20160704-crestronapi.sql
+++ resources/dbmigration/108-20160704-crestronapi.sql
@@ -2,7 +2,7 @@ ALTER TABLE `livefeeds`
 ADD `pin` int(10) NULL;
 
 ALTER TABLE `livefeeds`
-ADD UNIQUE `uq-pin` (`pin`)
+ADD UNIQUE `uq-pin` (`pin`);
 
 ALTER TABLE `organizations`
 ADD `islivepinenabled` int(11) NOT NULL DEFAULT '0';
