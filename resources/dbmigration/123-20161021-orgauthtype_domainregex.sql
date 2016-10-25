30fd0bbf1bab61fad71bce74644671c548f28e40
Peter SZTANOJEV <sztanpet@gmail.com>
2016-10-25 10:32:11 +0200

    [completed: 2019] db migracios script, roszul volt irva az oszlop neve

--- resources/dbmigration/123-20161021-orgauthtype_domainregex.sql
+++ resources/dbmigration/123-20161021-orgauthtype_domainregex.sql
@@ -1,5 +1,5 @@
 ALTER TABLE `organizations_authtypes`
-CHANGE `domain` `domainregex` text NOT NULL AFTER `type`;
+CHANGE `domains` `domainregex` text NOT NULL AFTER `type`;
 
 ALTER TABLE `organizations_directories`
 CHANGE `domains` `domainregex` text NOT NULL AFTER `password`;
