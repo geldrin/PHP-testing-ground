d8e0579415dd4236aa21b275045577328e01bcc9
geldrin <g3ldrin@gmail.com>
2017-02-16 09:47:19 +0100

    object initialization in one row (php 6,7)

---
+++ PHP-testing-ground/stuff/obj_init_adv.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * 
+ * Init and call methods of the same object in one expression.
+ * 
+ */
+
+$a = $b = $c = null;
+
+// easy mode
+$a = new Appul();   // instantiate
+$a->boop = "Heya!"; // set variable
+$a->greet();        // call method
+
+// hard mode (all in one expression)
+$b = Appul::getInstance()->set("Howdy, partner!")->greet(); // PHP 5.6+ (needs an extra class returning an instance)
+($b = new Appul())->set("Howdy, partner!")->greet();        // PHP 7.0+
+
+// hard mode #2
+// FATAL ERROR (Cannot use temporary expression in write context)
+//($c = new Appul())->$boop = "Bye!";
+//$c->greet();
+
+echo "---------\n";
+var_dump($a,$b,$c);
+
+/**
+ * test class
+ */
+class Appul {
+    public $boop = "";
+    
+    static function getInstance() { return new Appul(); } // for PHP 5.6<=
+    function set($s) { $this->boop = $s; return $this; }
+    function greet() { print_r("{$this->boop}\n"); return $this; } // return can be omitted in PHP 7.0+
+}
