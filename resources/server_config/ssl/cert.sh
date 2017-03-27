95a92ce06b6f4ef9a1ac2f7c80be77f18272de13
root <root@vsq-conv>
2017-03-27 14:40:35 +0200

    cert gen update

--- resources/server_config/ssl/cert.sh
+++ resources/server_config/ssl/cert.sh
@@ -11,18 +11,29 @@ openssl_config="openssl.cnf"
 
 if [ ! -f $openssl_config ];
 then
-    echo "ERROR: OpenSSL config does note exists"
+    echo "ERROR: OpenSSL config does not exists"
     exit
 fi
 
 keyfile=${domain}.key
 csrfile=${domain}.csr
+certfile=${domain}.crt
 
 # Generate key
 openssl genrsa -out ${keyfile} 2048
 
 # Generate CSR
-openssl req -new -out ${csrfile} -key ${keyfile} -sha256 -config openssl.cnf
+openssl req -new -out ${csrfile} -key ${keyfile} -sha256 -config openssl.cnf -batch
 
 # Verify CSR
 openssl req -text -noout -verify -in ${csrfile}
+
+# Generate certificate
+#openssl req -new -x509 -key ${keyfile} -sha256 -out ${certfile} -config openssl.cnf -days 3650 -batch
+openssl req -x509 -days 3650 -in ${csrfile} -key ${keyfile} -sha256 -config openssl.cnf -out ${certfile} -extensions 'v3_req' -batch
+
+# Print cert
+openssl x509 -in ${certfile} -text -noout
+
+# Generate PEM with Key
+cat ${certfile} ${keyfile} > ${domain}.pem
